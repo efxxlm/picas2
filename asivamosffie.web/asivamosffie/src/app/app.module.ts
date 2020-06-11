981cfdcb6d521f96f75770949e2a996403c55c06
Steward <johans.galeanot@hotmail.com>
2020-06-11 08:42:14 -0500

    instalo angular material y flexboxgrid

--- asivamosffie.web/asivamosffie/src/app/app.module.ts
+++ asivamosffie.web/asivamosffie/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LayoutComponent } from './layout/layout.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { CoreModule } from './core/core.module';
     HttpClientModule,
     SharedModule,
     CoreModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- asivamosffie.web/asivamosffie/src/app/shared/shared.module.ts
+++ asivamosffie.web/asivamosffie/src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { MaterialModule } from './../material/material.module';
+
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -17,6 +19,7 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     CommonModule,
     RouterModule,
+    MaterialModule,
   ]
 })
 export class SharedModule { }
