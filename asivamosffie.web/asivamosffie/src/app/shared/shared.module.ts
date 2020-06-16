73b7b48474f207080aa8b47373bd780bbbdb35fe
Johan Galeano <johans.galeanot@hotmail.com>
2020-06-16 14:18:14 -0500

    botón cerrar sesion

--- asivamosffie.web/asivamosffie/src/app/shared/shared.module.ts
+++ asivamosffie.web/asivamosffie/src/app/shared/shared.module.ts
@@ -8,17 +8,20 @@ import { MaterialModule } from './../material/material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
     ModalDialogComponent,
+    NavbarComponent,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     ModalDialogComponent,
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
