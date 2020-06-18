81e9449c7aa2a5f487b999684e79aa95b71aefd1
Johan Galeano <johans.galeanot@hotmail.com>
2020-06-18 13:18:15 -0500

    HU 2.3

--- asivamosffie.web/asivamosffie/src/app/app-routing.module.ts
+++ asivamosffie.web/asivamosffie/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CambiarContrasenaModule } from './_pages/cambiar-contrasena/cambiar-contrasena.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules, PreloadingStrategy } from '@angular/router';
 
@@ -20,6 +21,10 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('./_pages/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'cambiarContrasena',
+        loadChildren: () => import('./_pages/cambiar-contrasena/cambiar-contrasena.module').then(m => m.CambiarContrasenaModule)
       },
     ]
 
