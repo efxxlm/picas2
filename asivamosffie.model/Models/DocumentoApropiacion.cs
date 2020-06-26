239528384c99967f89f53a17d3fa3df38005bb33
julian1010011010 <47957027+julian1010011010@users.noreply.github.com>
2020-06-26 09:23:38 -0500

    CAmbio model

--- asivamosffie.model/Models/DocumentoApropiacion.cs
+++ asivamosffie.model/Models/DocumentoApropiacion.cs
@@ -12,6 +12,8 @@ namespace asivamosffie.model.Models
         public string TipoDocumentoCodigo { get; set; }
         public string NumeroDocumento { get; set; }
         public DateTime Fecha { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string UsuarioCreacion { get; set; }
 
         public virtual Aportante Aportante { get; set; }
     }
---
+++ asivamosffie.model/Models/RegistroPresupuestal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asivamosffie.model.Models
+{
+    public partial class RegistroPresupuestal
+    {
+        public RegistroPresupuestal()
+        {
+            ControlRecurso = new HashSet<ControlRecurso>();
+        }
+
+        public int RegistroPresupuestalId { get; set; }
+        public int AportanteId { get; set; }
+        public string NumeroRp { get; set; }
+        public DateTime FechaRp { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+
+        public virtual Aportante Aportante { get; set; }
+        public virtual ICollection<ControlRecurso> ControlRecurso { get; set; }
+    }
+}
