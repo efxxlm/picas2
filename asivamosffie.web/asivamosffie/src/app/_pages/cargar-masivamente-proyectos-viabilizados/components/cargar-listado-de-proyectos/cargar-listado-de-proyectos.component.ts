2c03e04e0b7e04c246badd37b78f5b081ce679dd
juan <ivolucion@outlook.es>
2020-07-04 21:02:33 -0500

    v0

--- asivamosffie.web/asivamosffie/src/app/_pages/cargar-masivamente-proyectos-viabilizados/components/cargar-listado-de-proyectos/cargar-listado-de-proyectos.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/cargar-masivamente-proyectos-viabilizados/components/cargar-listado-de-proyectos/cargar-listado-de-proyectos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ProjectService, RespuestaProyecto } from 'src/app/core/_services/project/project.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component';
 
 
 @Component({
@@ -11,8 +14,9 @@ export class CargarListadoDeProyectosComponent implements OnInit {
 
   fileListaProyectos: FormControl;
   archivo: string;
+  boton:string="Cargar";
 
-  constructor() {
+  constructor( private projectService: ProjectService,public dialog: MatDialog,) {
     this.declararInputFile();
   }
 
@@ -27,9 +31,52 @@ export class CargarListadoDeProyectosComponent implements OnInit {
     const inputNode: any = document.getElementById('file');
     this.archivo = inputNode.files[0].name;
   }
+  openDialog(modalTitle: string, modalText: string) {
+    let dialogRef =this.dialog.open(ModalDialogComponent, {
+      width: '28em',
+      data: { modalTitle, modalText }
+    });   
+  }
+  si()
+  {
+    console.log("Si");
+  }
 
   enviarListaProyectos() {
     if (this.fileListaProyectos.valid) {
+      this.boton="Aguarde un momento, estamos procesando el archivo";
+      const inputNode: any = document.getElementById('file');    
+      this.projectService.uploadFileProject(inputNode.files[0]).subscribe(
+        response => {
+          console.log(response);        
+          let respuestaCargue:RespuestaProyecto=response.data;
+          let strOpciones ="";
+          if(respuestaCargue.cantidadDeRegistrosValidos>0)
+          {
+            strOpciones="<br><b>¿Desea realizar el cargue de los proyectos validos?</b><br>"+
+            "<button type='button' (click)='Si()'>Si</button>   <button type='button'>No</button>";
+          }
+          this.openDialog('Validación de registro', 
+          "<br>Número de registros en el archivo:<b>"+respuestaCargue.cantidadDeRegistros+"</b><br>"+
+          "Número de registros validos: <b>"+respuestaCargue.cantidadDeRegistrosValidos+"</b><br>"+
+          "Número de registros inválidos: <b>"+respuestaCargue.cantidadDeRegistrosInvalidos+"</b><br>"
+          );  
+         
+        },
+        error => {
+          console.log(<any>error);
+          let mensaje: string;
+            if (error.error.message){
+              mensaje = error.error.message;
+            }else {
+              mensaje = error.message;
+            }
+            this.openDialog('Error', mensaje);
+            
+          },
+        () =>{
+          this.boton="Cargar";
+        });
       console.log(this.fileListaProyectos.value);
     }
   }
---
+++ asivamosffie.web/asivamosffie/src/app/core/_services/project/project.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Respuesta } from '../autenticacion/autenticacion.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor(private http: HttpClient) {  }
+
+  public uploadFileProject(archivoParaSubir: File) {
+    const formData = new FormData(); 
+    formData.append('file', archivoParaSubir, archivoParaSubir.name);
+    //si se requieren mas variables
+    //formData.append('proyectId',projectid);
+    const retorno = this.http.post<Respuesta>(`${environment.apiUrl}/Project/SetValidateMassiveLoadProjects`,formData);
+    return retorno;
+  }
+
+  
+}
+export interface RespuestaProyecto{
+  cantidadDeRegistros: number,
+  cantidadDeRegistrosInvalidos: number,
+  cantidadDeRegistrosValidos: number,
+  llaveConsulta: string
+}
