af9c3891b5ff8b0d603ade28ec26f8e17abf5d91
Johan Galeano <johans.galeanot@hotmail.com>
2020-07-09 14:17:02 -0500

    gestionarFuentes

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-fuentes-de-financiacion/components/registrar/registrar.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-fuentes-de-financiacion/components/registrar/registrar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormArray, ControlValueAccessor, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -6,24 +6,17 @@ import { FormBuilder, Validators, FormArray, ControlValueAccessor, FormGroup, Fo
   templateUrl: './registrar.component.html',
   styleUrls: ['./registrar.component.scss']
 })
-export class RegistrarComponent 
-{
+export class RegistrarComponent implements OnInit {
   addressForm: FormGroup;
 
-  nombresAportantes: any;
-  fuentesDeRecursosLista: any;
-  bancos: any;
-  VigenciasAporte: any;
+  nombresAportantes = [ {name: 'FFIE', value: '1'}, {name: 'FFIE', value: '2'}, {name: 'FFIE', value: '3'}, {name: 'FFIE', value: '4'} ];
+  fuentesDeRecursosLista = [ {name: 'Recursos propios', value: '1'}, {name: 'Recursos', value: '2'}, {name: 'Recursos', value: '3'}, {name: 'Recursos', value: '4'} ];
+  bancos = [ {name: 'banco 1', value: '1'}, {name: 'banco 2', value: '2'}, {name: 'banco 3', value: '3'}, {name: 'banco 4', value: '4'} ];
+  VigenciasAporte = [ 2015, 2016, 2017, 2019, 2020, 2021, 2022 ];
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-
-    this.nombresAportantes = [ {name: 'FFIE', value: '1'}, {name: 'FFIE', value: '2'}, {name: 'FFIE', value: '3'}, {name: 'FFIE', value: '4'} ];
-    this.fuentesDeRecursosLista = [ {name: 'Recursos propios', value: '1'}, {name: 'Recursos propios', value: '2'}, {name: 'Recursos propios', value: '3'}, {name: 'Recursos propios', value: '4'} ];
-    this.bancos = [ {name: 'banco 1', value: '1'}, {name: 'banco 2', value: '2'}, {name: 'banco 3', value: '3'}, {name: 'banco 4', value: '4'} ];
-    this.VigenciasAporte = [ 2015, 2016, 2017, 2019, 2020, 2021, 2022 ];
-
 
     this.addressForm = this.fb.group({
       nombreAportante: [null, Validators.required],
@@ -38,14 +31,7 @@ export class RegistrarComponent
             Validators.required, Validators.minLength(2), Validators.maxLength(2)])
           ],
           cuantasVigencias: [null, Validators.required],
-          vigencias: this.fb.array([
-            this.fb.group({
-              vigenciaAportante: [null, Validators.required],
-              valorVigencia: [null, Validators.compose([
-                Validators.required, Validators.minLength(10), Validators.maxLength(10)])
-              ]
-              })
-          ]),
+          vigencias: this.fb.array([]),
           cuentasBancaria: this.fb.array([
             this.fb.group({
               numeroCuenta: [null, Validators.compose([
@@ -60,27 +46,27 @@ export class RegistrarComponent
               tipoCuenta: ['free', Validators.required],
               banco: [null, Validators.required],
               extra: ['free', Validators.required]
-              })
+            })
           ]),
         })]),
     });
     console.log(this.addressForm);
   }
 
-  get vigencias1() {    
-    let control=this.addressForm.get('fuenteRecursosArray') as FormArray;    
-    return control.controls[0].get('vigencias') as FormArray;
-  }
   get fuenteRecursosArray() {
     return this.addressForm.get('fuenteRecursosArray') as FormArray;
   }
+  get vigencias1() {
+    const control = this.addressForm.get('fuenteRecursosArray') as FormArray;
+    return control.controls[0].get('vigencias') as FormArray;
+  }
   get cuentasBancaria() {
-    let control=this.addressForm.get('fuenteRecursosArray') as FormArray;
+    const control = this.addressForm.get('fuenteRecursosArray') as FormArray;
     return control.controls[0].get('cuentasBancaria') as FormArray;
   }
 
-  CambioNumerovigencia() {
-    const FormNumvigencias = this.addressForm.value;
+  CambioNumerovigencia(j: number) {
+    const FormNumvigencias = this.addressForm.value.fuenteRecursosArray[j];
     if (FormNumvigencias.cuantasVigencias > this.vigencias1.length && FormNumvigencias.cuantasVigencias < 100) {
       while (this.vigencias1.length < FormNumvigencias.cuantasVigencias) {
         this.vigencias1.push(this.createVigencia());
@@ -114,10 +100,10 @@ export class RegistrarComponent
       tipoCuenta: ['free', Validators.required],
       banco: [null, Validators.required],
       extra: ['free', Validators.required]
-      });
+    });
   }
 
-  crearFuente():FormGroup{
+  crearFuente(): FormGroup {
     return this.fb.group({
       fuenteRecursos: [null, Validators.required],
       valorFuenteRecursos: [null, Validators.compose([
@@ -139,9 +125,9 @@ export class RegistrarComponent
           tipoCuenta: ['free', Validators.required],
           banco: [null, Validators.required],
           extra: ['free', Validators.required]
-          })
+        })
       ]),
-    })
+    });
   }
 
   createVigencia(): FormGroup {
@@ -150,7 +136,7 @@ export class RegistrarComponent
       valorVigencia: [null, Validators.compose([
         Validators.required, Validators.minLength(10), Validators.maxLength(10)])
       ]
-      });
+    });
   }
 
   borrarArray(borrarForm: any, i: number) {
