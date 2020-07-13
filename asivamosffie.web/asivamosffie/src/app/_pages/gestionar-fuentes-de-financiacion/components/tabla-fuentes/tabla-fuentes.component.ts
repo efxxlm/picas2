1b63a7cb9800d572425e863f24993742cfbe6e21
juan <ivolucion@outlook.es>
2020-07-13 08:18:19 -0500

    for build

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-fuentes-de-financiacion/components/tabla-fuentes/tabla-fuentes.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-fuentes-de-financiacion/components/tabla-fuentes/tabla-fuentes.component.ts
@@ -71,7 +71,13 @@ export class TablaFuentesComponent implements OnInit {
   editarFuente(e: number) {
     console.log(e);
   }
+  editarAcuerdo(e: number) {
+    console.log(e);
+  }
   eliminarFuente(e: number) {
+    console.log(e);
+  }
+  eliminarAcuerdo(e: number) {
     console.log(e);
   }
   controlRecursosFuente(e: number) {
