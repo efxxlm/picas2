8528f00dd9c9e5885f709d0692c22864548be151
jsorozcof <35968382+jsorozcof@users.noreply.github.com>
2020-07-16 20:58:17 -0500

    Update service source funding v2

--- asivamosffie.services/SourceFundingService.cs
+++ asivamosffie.services/SourceFundingService.cs
@@ -39,31 +39,14 @@ namespace asivamosffie.services
         public async Task<Respuesta> Insert(FuenteFinanciacion fuentefinanciacion)
         {
             Respuesta respuesta = new Respuesta();
-            var contributor = await _context.FuenteFinanciacion.Where(r => r.AportanteId == fuentefinanciacion.AportanteId).ToListAsync();
-            //Pendiente, No mostrar la fuente que ya fue utilizada
-            if (contributor != null)
-            { 
-            
-            }
 
-            Console.WriteLine(contributor);
-         
             try
             {
                 if (fuentefinanciacion != null)
                 {
-                  var SF = new FuenteFinanciacion()
-                    {
-                       //CantVigencias = fuentefinanciacion.CantVigencias,
-                       //FechaCreacion = fuentefinanciacion.FechaCreacion,
-                       //UsuarioCreacion = Helpers.Helpers.ConvertToUpercase(fuentefinanciacion.UsuarioCreacion),
-                       //FuenteRecursosCodigo = Helpers.Helpers.ConvertToUpercase(fuentefinanciacion.FuenteRecursosCodigo),
-                       //ValorFuente = fuentefinanciacion.ValorFuente,
-                       //FuenteFinanciacionId = fuentefinanciacion.FuenteFinanciacionId,
-                       
-
-                  };
-                    _context.Add(SF);
+                    fuentefinanciacion.UsuarioCreacion = "forozco";
+                    fuentefinanciacion.FechaCreacion = DateTime.Now;
+                    _context.Add(fuentefinanciacion);
                     await _context.SaveChangesAsync();
                     respuesta = new Respuesta() { IsSuccessful = true, IsValidation = true, Data = fuentefinanciacion, Code = ConstantMessagesContributor.OperacionExitosa };
                 }
