9b76a2b2470c092d54bbf0e46eb5dff1703557a2
Faber Orozco <tkferb_22@hotmail.com>
2020-07-29 15:37:08 +0000

    Updated ConstanCodigoAcciones 202007 1037

--- asivamosffie.services/Helpers/Constants/ConstanCodigoAcciones.cs
+++ asivamosffie.services/Helpers/Constants/ConstanCodigoAcciones.cs
@@ -29,6 +29,7 @@
         public const string Crear_Proceso_Seleccion = "18";
          public const string Crear_Editar_ProcesoSeleccion_Cronograma = "28";
         public const string Crear_Editar_ProcesoSeleccion_Grupo = "29";
+        public const string Crear_Editar_ProcesoSeleccion_Cotizacion = "30";
         public const string Crear_Editar_Cofinanciacion = "31";
     }
 }
