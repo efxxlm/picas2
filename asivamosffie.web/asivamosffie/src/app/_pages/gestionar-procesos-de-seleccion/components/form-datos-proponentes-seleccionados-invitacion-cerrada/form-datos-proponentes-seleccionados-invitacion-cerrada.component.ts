cd7baaa347f7bd9b846ec9149fa87c9acab64105
Johan Galeano <johans.galeanot@hotmail.com>
2020-08-05 10:11:27 -0500

    invitación cerrada

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-procesos-de-seleccion/components/form-datos-proponentes-seleccionados-invitacion-cerrada/form-datos-proponentes-seleccionados-invitacion-cerrada.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-procesos-de-seleccion/components/form-datos-proponentes-seleccionados-invitacion-cerrada/form-datos-proponentes-seleccionados-invitacion-cerrada.component.ts
@@ -7,8 +7,42 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./form-datos-proponentes-seleccionados-invitacion-cerrada.component.scss']
 })
 export class FormDatosProponentesSeleccionadosInvitacionCerradaComponent {
+
+  nombresProponentesList: string[] = ['Nathalia Aranda', 'Andres Montealegre', 'Ana Sandoval'];
+
   addressForm = this.fb.group({
-    
+    cuantosProponentes: [null, Validators.compose([
+      Validators.required, Validators.minLength(1), Validators.maxLength(2)])
+    ],
+    nombresProponentes: [null, Validators.required],
+    tipoProponente: [null, Validators.required],
+    nombre: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(100)])
+    ],
+    numeroIdentificacon: [null, Validators.compose([
+      Validators.required, Validators.minLength(10), Validators.maxLength(12)])
+    ],
+    representanteLegal: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(100)])
+    ],
+    cedulaRepresentanteLegal: [null, Validators.compose([
+      Validators.required, Validators.minLength(10), Validators.maxLength(12)])
+    ],
+    departamentoRepresentanteLegal: [null, Validators.required],
+    municipioRepresentanteLegal: [null, Validators.required],
+    direccionPrincipalRepresentanteLegal: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(100)])
+    ],
+    telefonoRepresentanteLegal: [null, Validators.compose([
+      Validators.required, Validators.minLength(10), Validators.maxLength(10)])
+    ],
+    correoRepresentanteLegal: [null, [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50),
+      Validators.email,
+      Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/)
+    ]],
   });
 
   constructor(private fb: FormBuilder) {}
