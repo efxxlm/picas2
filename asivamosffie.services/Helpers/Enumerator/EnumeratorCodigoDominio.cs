6c702d802a1bc9a6ca66d8136cce52cd67509de7
Faber Orozco <tkferb_22@hotmail.com>
2020-08-06 22:49:05 +0000

    Updated EnumeratorCodigoDominio

--- asivamosffie.services/Helpers/Enumerator/EnumeratorCodigoDominio.cs
+++ asivamosffie.services/Helpers/Enumerator/EnumeratorCodigoDominio.cs
@@ -3,7 +3,8 @@ namespace asivamosffie.services.Helpers.Enumerator
 {
     public class OrigenArchivoCargue {
 
-        public const string Proyecto = "1";
+         public const string Proyecto = "1";
+        public const string OrdeELegibilidad = "2";
 
     }
 }
