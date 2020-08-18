2d1ed03551a8c8347be4f28e5ec449fc5585d0fb
julian1010011010 <47957027+julian1010011010@users.noreply.github.com>
2020-08-18 17:13:43 -0500

    Cambio Model
    SesionInvitado Campos Auditoria

--- asivamosffie.services/devAsiVamosFFIEContext.cs
+++ asivamosffie.services/devAsiVamosFFIEContext.cs
@@ -2134,8 +2134,20 @@ namespace asivamosffie.model.Models
                     .HasMaxLength(200)
                     .IsUnicode(false);
 
+                entity.Property(e => e.FechaCreacion).HasColumnType("datetime");
+
+                entity.Property(e => e.FechaModificacion).HasColumnType("datetime");
+
                 entity.Property(e => e.Nombre)
                     .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UsuarioCreacion)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UsuarioModificacion)
                     .HasMaxLength(200)
                     .IsUnicode(false);
 
