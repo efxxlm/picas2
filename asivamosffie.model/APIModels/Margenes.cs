bfd589848456d44fc5561760da0b7eb7d89f3911
julian1010011010 <47957027+julian1010011010@users.noreply.github.com>
2020-08-19 14:19:56 -0500

    PDF GENERAR

---
+++ asivamosffie.model/APIModels/Margenes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asivamosffie.model.APIModels
+{
+    public class Margenes
+    {
+        public float Superior
+        {
+            get;
+            set;
+        }
+        public float Inferior
+        {
+            get;
+            set;
+        }
+        public float Izquierda
+        {
+            get;
+            set;
+        }
+        public float Derecha
+        {
+            get;
+            set;
+        } 
+    }
+}
--- asivamosffie.services/Helpers/Helpers.cs
+++ asivamosffie.services/Helpers/Helpers.cs
@@ -9,178 +9,183 @@ using System.Text;
 using asivamosffie.model.Models;
 using asivamosffie.services.Helpers.Enumerator;
 using System.Text.RegularExpressions;
+using asivamosffie.model.APIModels;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using asivamosffie.model.Models;
+using asivamosffie.services.Helpers.Enumerator;
+using System.Text.RegularExpressions;
+using asivamosffie.model.APIModels;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.tool.xml.pipeline.html;
+using iTextSharp.tool.xml.html;
+using iTextSharp.tool.xml.pipeline.css;
+using iTextSharp.tool.xml.pipeline.end;
+using iTextSharp.tool.xml.pipeline;
 
 namespace asivamosffie.services.Helpers
 {
-    public class Helpers
+    public class PDF
     {
         private readonly devAsiVamosFFIEContext _context;
 
-        public Helpers(devAsiVamosFFIEContext context)
+        public PDF(devAsiVamosFFIEContext context)
         {
             _context = context;
         }
 
-  
 
-        public static string encryptSha1(string password)
+        public byte[] convertir(string contenido, string encabezado, string pie, Margenes margenes)
         {
+            contenido = contenido.Replace("\r\n", "");
+            contenido = contenido.Replace("\r\n", "");
+            contenido = contenido.Replace("\r\n", "");
+
+            FontFactory.RegisterDirectories();
+
+            double margenIzquierdo = CentimetrosAMedidaPDF(margenes.Izquierda);
+            double margenDerecho = CentimetrosAMedidaPDF(margenes.Derecha);
+            double margenSuperior = CentimetrosAMedidaPDF(margenes.Superior);
+            double margenInferior = CentimetrosAMedidaPDF(margenes.Inferior);
+
+            Document document = new Document();
+            iTextSharp.text.Rectangle z = PageSize.LETTER;
+            document = new Document(z, (float)margenIzquierdo, (float)margenDerecho, (float)margenSuperior, (float)margenInferior);
 
 
-            UTF8Encoding enc = new UTF8Encoding();
-            byte[] data = enc.GetBytes(password);
-            byte[] result;
+            EventosPdf e = new EventosPdf();
+            e.Iniciar(encabezado, pie, (float)margenes.Superior, (float)margenes.Inferior);
+            MemoryStream stream = new MemoryStream();
 
-            SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider();
+            PdfWriter writer = PdfWriter.GetInstance(document, stream);
+            writer.PageEvent = e;
 
-            result = sha.ComputeHash(data);
+            document.Open();
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < result.Length; i++)
-            {
+            HtmlPipelineContext htmlContext = new HtmlPipelineContext(null);
 
-                // Convertimos los valores en hexadecimal
-                // cuando tiene una cifra hay que rellenarlo con cero
-                // para que siempre ocupen dos dígitos.
-                if (result[i] < 16)
-                {
-                    sb.Append("0");
-                }
-                sb.Append(result[i].ToString("x"));
-            }
-            return sb.ToString().ToUpper();
+            htmlContext.SetTagFactory(Tags.GetHtmlTagProcessorFactory());
+
+            ICSSResolver cssResolver = XMLWorkerHelper.GetInstance().GetDefaultCssResolver(true);
+
+            IPipeline pipeline = new CssResolverPipeline(cssResolver, new HtmlPipeline(htmlContext, new PdfWriterPipeline(document, writer)));
+
+            XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, new StringReader(contenido));
+
+            document.Close();
+            byte[] retorno = stream.ToArray();
+
+            return retorno;
         }
 
-        public static string CleanStringInput(string text)//ÁÉÍÓÚ //
+        public double CentimetrosAMedidaPDF(double centimetros)
+        {
+            return (double)(centimetros * 0.393701 * 72);
+        }
+
+        class EventosPdf : PdfPageEventHelper
         {
 
-            char[] replacement = { 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-            char[] accents = { 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'é', 'è', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'ö', 'õ', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Á', 'É', 'Í', 'Ó', 'Ú', '/', '.', ',', '@', '_', '(', ')', ':', ';' };
+            public string Encabezado;
+            public string Pie;
+            public float MargenSuperior;
+            public float MargenInferior;
+            protected ElementList header;
+            protected ElementList footer;
 
-            if (text != null)
+            public void Iniciar(string encabezado, string pie, float margenSuperior, float margenInferior)
             {
-                for (int i = 0; i < accents.Length; i++)
+                this.Encabezado = encabezado;
+                this.MargenSuperior = margenSuperior;
+                this.MargenInferior = margenInferior;
+                this.Pie = pie;
+                if (!string.IsNullOrEmpty(encabezado))
                 {
-                    text = text.Replace(accents[i], replacement[i]);
+                    header = XMLWorkerHelper.ParseToElementList(encabezado, null);
+                }
+                if (!string.IsNullOrEmpty(pie))
+                {
+                    footer = XMLWorkerHelper.ParseToElementList(pie, null);
                 }
             }
 
-            return text;
-        }
-
-        public static object ConvertToUpercase(object dataObject)
-        {
-            try
+            public override void OnStartPage(PdfWriter writer, Document document)
             {
-                object newObject = new System.Dynamic.ExpandoObject();
-                char[] replacement = { 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-                char[] accents = { 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'é', 'è', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'ö', 'õ', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Á', 'É', 'Í', 'Ó', 'Ú', '/', '.', ',', '@', '_', '(', ')', ':', ';' };
 
-                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(dataObject))
+                if (Encabezado != null)
                 {
 
-                    string name = descriptor.Name;
-                    object value = descriptor.GetValue(dataObject);
-                    for (int i = 0; i < accents.Length; i++)
+                }
+            }
+
+            public override void OnEndPage(PdfWriter writer, Document document)
+            {
+
+                try
+                {
+                    ColumnText ct = new ColumnText(writer.DirectContent);
+                    if (header != null)
                     {
-                        ((IDictionary<String, Object>)newObject).Add(name,
-                         value =
-                         (
-                             descriptor.PropertyType.Name == "String" && value.ToString() != null ?
-                             Convert.ToString(value.ToString().Replace(accents[i], replacement[i])).ToUpper() : value
-                         ));
+
+                        ct.SetSimpleColumn(new Rectangle(36, 0, 559, document.Top + document.TopMargin - 10));
+
+                        foreach (IElement e in header)
+                        {
+                            ct.AddElement(e);
+                        }
+                        ct.Go();
+                    }
+                    if (footer != null)
+                    {
+                        ct.SetSimpleColumn(new Rectangle(36, 0, 559, 80));
+
+                        foreach (IElement e in footer)
+                        {
+                            ct.AddElement(e);
+                        }
+                        ct.Go();
                     }
                 }
-
-
-                return newObject; //Newtonsoft.Json.JsonConvert.SerializeObject();
+                catch (Exception de)
+                {
+                    throw de;
+                }
             }
-            catch (Exception ex)
-            {
-                return ex.Message.ToString();
-            }
+
         }
 
-        public static bool EnviarCorreo(string pDestinatario, string pAsunto, string pMensajeHtml, string pCorreoLocal, string pPassword, string pStrSmtpServerV, int pSmtpPort)
+        public class ColumnTextElementHandler : IElementHandler
         {
-            try
+            public ColumnTextElementHandler(ColumnText ct)
             {
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient(pStrSmtpServerV);
-
-                mail.From = new MailAddress(pCorreoLocal);
-                mail.To.Add(pDestinatario);
-                mail.Subject = pAsunto;
-                mail.IsBodyHtml = true;
-
-                mail.Body = pMensajeHtml;
-                SmtpServer.Port = pSmtpPort;
-                SmtpServer.Credentials = new NetworkCredential(pCorreoLocal, pPassword);
-                SmtpServer.EnableSsl = false;
-                SmtpServer.Send(mail);
-            }
-            catch (Exception e)
-            {
-                return false;
+                this.ct = ct;
             }
 
-            return true;
+            ColumnText ct = null;
+
+            public void Add(IWritable w)
+            {
+                if (w is WritableElement)
+                {
+                    foreach (IElement e in ((WritableElement)w).Elements())
+                    {
+                        ct.AddElement(e);
+                    }
+                }
+            }
         }
 
-        public static string GeneratePassword(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, bool includeSpaces, int lengthOfPassword)
-        {
-            const int MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS = 2;
-            const string LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
-            const string UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            const string NUMERIC_CHARACTERS = "0123456789";
-            const string SPECIAL_CHARACTERS = @"!#$%*\";
-            const string SPACE_CHARACTER = " ";
-            const int PASSWORD_LENGTH_MIN = 8;
-            const int PASSWORD_LENGTH_MAX = 128;
-
-            if (lengthOfPassword < PASSWORD_LENGTH_MIN || lengthOfPassword > PASSWORD_LENGTH_MAX)
-            {
-                return "Password length must be between 8 and 128.";
-            }
-
-            string characterSet = "";
-
-            if (includeLowercase)
-            {
-                characterSet += LOWERCASE_CHARACTERS;
-            }
-
-            if (includeUppercase)
-            {
-                characterSet += UPPERCASE_CHARACTERS;
-            }
-
-            if (includeNumeric)
-            {
-                characterSet += NUMERIC_CHARACTERS;
-            }
-
-            if (includeSpecial)
-            {
-                characterSet += SPECIAL_CHARACTERS;
-            }
-
-            if (includeSpaces)
-            {
-                characterSet += SPACE_CHARACTER;
-            }
-
-            char[] password = new char[lengthOfPassword];
-            int characterSetLength = characterSet.Length;
-
-            System.Random random = new System.Random();
-            for (int characterPosition = 0; characterPosition < lengthOfPassword; characterPosition++)
-            {
-                password[characterPosition] = characterSet[random.Next(characterSetLength - 1)];
-            }
-            Random randomw = new Random();
-            string def = "Az-" + randomw.Next(5).ToString();
-            return string.Join(null, password) + def;
-        }
     }
 }
