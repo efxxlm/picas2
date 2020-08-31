44573c12d44843ceb4a7d514f66c7ae808fa8e3f
Johan Galeano <johans.galeanot@hotmail.com>
2020-08-31 14:24:07 -0500

    table

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/generar-disponibilidad-presupuestal-routing.module.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/generar-disponibilidad-presupuestal-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MainComponent } from './components/main/main.component';
+import { MenuGenerarDisponibilidadComponent } from './components/menu-generar-disponibilidad/menu-generar-disponibilidad.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MenuGenerarDisponibilidadComponent
+  },
+  {
+    path: 'DDP/:id',
+    component: MenuGenerarDisponibilidadComponent
   }
 ];
 
--- asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/generar-disponibilidad-presupuestal.module.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/generar-disponibilidad-presupuestal.module.ts
@@ -7,9 +7,11 @@ import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { GenerarDisponibilidadPresupuestalRoutingModule } from './generar-disponibilidad-presupuestal-routing.module';
+import { MenuGenerarDisponibilidadComponent } from './components/menu-generar-disponibilidad/menu-generar-disponibilidad.component';
+import { TablaConValidacionPresupuestalComponent } from './components/tabla-con-validacion-presupuestal/tabla-con-validacion-presupuestal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuGenerarDisponibilidadComponent, TablaConValidacionPresupuestalComponent],
   imports: [
     CommonModule,
     GenerarDisponibilidadPresupuestalRoutingModule,
