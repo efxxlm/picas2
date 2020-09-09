c9751f3152759a9dd7af4fdf935d0b4dd93a906f
juan <ivolucion@outlook.es>
2020-09-09 15:09:12 -0500

    menu integrado

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/components/menu-generar-disponibilidad/menu-generar-disponibilidad.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/components/menu-generar-disponibilidad/menu-generar-disponibilidad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DisponibilidadPresupuestalService } from 'src/app/core/_services/disponibilidadPresupuestal/disponibilidad-presupuestal.service';
 
 @Component({
   selector: 'app-menu-generar-disponibilidad',
@@ -8,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
 export class MenuGenerarDisponibilidadComponent implements OnInit {
 
   verAyuda = false;
+  listaDisponibilidades: any[]=[];
 
-  constructor() { }
+  constructor(private disponibilidadServices: DisponibilidadPresupuestalService) {}
 
   ngOnInit(): void {
+    this.disponibilidadServices.GetListGenerarDisponibilidadPresupuestal().subscribe(respuesta => 
+      {
+        this.listaDisponibilidades=respuesta;
+        
+        this.listaDisponibilidades.forEach(element => {
+          //determino si esta completo o incompleto
+          
+          if(element.nombreEstado=='Con validación presupuestal')
+          {            
+            let cantcompleto=0;
+            element.disponibilidadPresupuestal.forEach(element2 => {
+              if(element2.estadoRegistro)
+              {
+                cantcompleto++;
+              }
+            });
+            if(cantcompleto==element.disponibilidadPresupuestal.length)
+            {
+              element.completo='Completo';
+            }
+            else{
+              if(element.disponibilidadPresupuestal.length>0)
+              {
+                element.completo='Incompleto';
+              }
+              else{
+                element.completo='';
+              }
+              
+            }
+          }
+         
+        });
+      }
+    );
   }
 
 }
