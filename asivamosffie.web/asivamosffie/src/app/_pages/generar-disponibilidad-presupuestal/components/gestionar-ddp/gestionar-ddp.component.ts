4f1f9423bb515a512ecd275a5d5d7740f7776dd5
juan <ivolucion@outlook.es>
2020-09-14 13:56:27 -0500

    fixes

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/components/gestionar-ddp/gestionar-ddp.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-disponibilidad-presupuestal/components/gestionar-ddp/gestionar-ddp.component.ts
@@ -4,6 +4,7 @@ import { DevolverPorValidacionComponent } from '../devolver-por-validacion/devol
 import { CancelarDdpComponent } from '../cancelar-ddp/cancelar-ddp.component';
 import { DisponibilidadPresupuestalService } from 'src/app/core/_services/disponibilidadPresupuestal/disponibilidad-presupuestal.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component';
 
 @Component({
   selector: 'app-gestionar-ddp',
@@ -11,18 +12,31 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./gestionar-ddp.component.scss']
 })
 export class GestionarDdpComponent implements OnInit {
-  detailavailabilityBudget: any[];
+  detailavailabilityBudget: any;
 
   constructor(public dialog: MatDialog,private disponibilidadServices: DisponibilidadPresupuestalService,
     private route: ActivatedRoute,
     private router: Router) { }
 
+  openDialog(modalTitle: string, modalText: string) {
+    this.dialog.open(ModalDialogComponent, {
+      width: '28em',
+      data: { modalTitle, modalText }
+    });
+  }
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.disponibilidadServices.GetDetailAvailabilityBudgetProyect(id).subscribe(listas => {
         console.log(listas);
-        this.detailavailabilityBudget=listas;
+        if(listas.length>0)
+        {
+          this.detailavailabilityBudget=listas[0];
+        }
+        else{
+          this.openDialog('','Error al intentar recuperar los datos de la solicitud, por favor intenta nuevamente.');
+        }
+        
       });
     }
   }
