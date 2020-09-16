5455fc4799bb4b0746c0850b71ddf270670f97b2
Faber Orozco <tkferb_22@hotmail.com>
2020-09-16 22:50:39 +0000

    Updated SesionParticipante.cs

--- asivamosffie.model/Models/SesionParticipante.cs
+++ asivamosffie.model/Models/SesionParticipante.cs
@@ -28,6 +28,8 @@ namespace asivamosffie.model.Models
 
         public virtual ComiteTecnico ComiteTecnico { get; set; }
         public virtual Usuario Usuario { get; set; }
+        
+        public string Nombres { get; set; }
         public virtual ICollection<Usuario> UsersIds { get; set; }
         public virtual ICollection<CompromisoSeguimiento> CompromisoSeguimiento { get; set; }
         public virtual ICollection<SesionComentario> SesionComentario { get; set; }
