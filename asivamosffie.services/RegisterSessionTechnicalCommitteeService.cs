9cd089df8c2e22720ff07037cceb86e485f753bf
DiegoEstupinan <daestupinans@dane.gov.co>
2020-09-22 15:09:57 -0500

    ajustar obligatorios

--- asivamosffie.services/RegisterSessionTechnicalCommitteeService.cs
+++ asivamosffie.services/RegisterSessionTechnicalCommitteeService.cs
@@ -167,6 +167,7 @@ namespace asivamosffie.services
                 string CrearEditar = "";
                 sesionComiteTemaOld.RequiereVotacion = true;
                 sesionComiteTemaOld.EstadoTemaCodigo = pSesionComiteTema.EstadoTemaCodigo;
+                sesionComiteTemaOld.EsAprobado = (pSesionComiteTema.EstadoTemaCodigo == "1") ? true : false;
                 sesionComiteTemaOld.UsuarioModificacion = pSesionComiteTema.UsuarioCreacion;
                 sesionComiteTemaOld.FechaModificacion = DateTime.Now;
                 sesionComiteTemaOld.RegistroCompleto = ValidarRegistroCompletoSesionComiteTema(sesionComiteTemaOld);
@@ -224,7 +225,7 @@ namespace asivamosffie.services
                 || string.IsNullOrEmpty(sesionComiteTemaOld.TiempoIntervencion.ToString())
                 //|| !string.IsNullOrEmpty(sesionComiteTemaOld.RutaSoporte)
                 || string.IsNullOrEmpty(sesionComiteTemaOld.Observaciones)
-                || sesionComiteTemaOld.EsAprobado == null
+                || ( sesionComiteTemaOld.RequiereVotacion == true && sesionComiteTemaOld.EsAprobado == null ) 
                 || sesionComiteTemaOld.RequiereVotacion == null
                 //|| sesionComiteTemaOld.EsProposicionesVarios == null
                 || sesionComiteTemaOld.GeneraCompromiso == null
