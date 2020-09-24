fcd45689e2b04dc57a10717e3c29324a69ccef0f
David Benitez <divist100@gmail.com>
2020-09-24 09:11:42 -0500

    modulo puesto

---
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-polizas-y-garantias/components/editar-observada-o-devuelta/editar-observada-o-devuelta.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component';
+
+@Component({
+  selector: 'app-editar-observada-o-devuelta',
+  templateUrl: './editar-observada-o-devuelta.component.html',
+  styleUrls: ['./editar-observada-o-devuelta.component.scss']
+})
+export class EditarObservadaODevueltaComponent implements OnInit {
+
+  addressForm = this.fb.group({
+    nombre: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(50)])
+    ],
+    numeroPoliza: [null, Validators.compose([
+      Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+    ],
+    numeroCertificado: [null, Validators.compose([
+      Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+    ],
+    fecha: [null, Validators.required],
+    vigenciaPoliza: [null, Validators.required],
+    vigenciaAmparo: [null, Validators.required],
+    valorAmparo: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(20)])
+    ],
+    polizasYSeguros: [null, Validators.required],
+    buenManejoCorrectaInversionAnticipo: [null, Validators.required],
+    estabilidadYCalidad: [null, Validators.required],
+    cumpleAsegurado: [null, Validators.required],
+    cumpleBeneficiario: [null, Validators.required],
+    cumpleAfianzado: [null, Validators.required],
+    reciboDePago: [null, Validators.required],
+    condicionesGenerales: [null, Validators.required],
+    fechaRevision: [null, Validators.required],
+    estadoRevision: [null, Validators.required],
+    observacionesGenerales: [null, Validators.required],
+  });
+
+  polizasYSegurosArray = [
+    { name: 'Buen manejo y correcta inversión del anticipo', value: '1' },
+    { name: 'Garantía de estabilidad y calidad de la obra', value: '2' },
+    { name: 'Póliza de cumplimiento', value: '3' },
+    { name: 'Garantía de estabilidad y calidad de la obra', value: '4' }
+  ];
+  estadoArray = [
+    { name: 'Devuelta', value: '1' }
+  ];
+
+  minDate: Date;
+
+  editorStyle = {
+    height: '100px'
+  };
+
+  config = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ align: [] }],
+    ]
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    public dialog: MatDialog
+  ) {
+    this.minDate = new Date();
+  }
+
+  ngOnInit(): void { }
+
+  // evalua tecla a tecla
+  validateNumberKeypress(event: KeyboardEvent) {
+    const alphanumeric = /[0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    return alphanumeric.test(inputChar) ? true : false;
+  }
+
+  maxLength(e: any, n: number) {
+    if (e.editor.getLength() > n) {
+      e.editor.deleteText(n, e.editor.getLength());
+    }
+  }
+
+  textoLimpio(texto: string) {
+    const textolimpio = texto.replace(/<[^>]*>/g, '');
+    return textolimpio.length;
+  }
+
+  openDialog(modalTitle: string, modalText: string) {
+    this.dialog.open(ModalDialogComponent, {
+      width: '28em',
+      data: { modalTitle, modalText }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.addressForm.value);
+    this.openDialog('', 'La información ha sido guardada exitosamente.');
+  }
+
+}
