7cb5cf105f59f8ad166deaf08df2b4059c764150
charlyg360ve <charlyg360ve@gmail.com>
2020-09-25 00:15:07 -0500

    fix-select

--- asivamosffie.web/asivamosffie/src/app/core/_services/common/common.service.ts
+++ asivamosffie.web/asivamosffie/src/app/core/_services/common/common.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { environment } from '../../../../environments/environment.prod';
+import { environment } from '../../../../environments/environment';
 import { map } from 'rxjs/operators';
 import { Observable, forkJoin } from 'rxjs';
 import { Usuario } from '../autenticacion/autenticacion.service';
