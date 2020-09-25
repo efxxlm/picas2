6d4aea9c696a8b2ac98b086d51af7f106a961b65
Camilo Daza <cdaza@ivolucion.com>
2020-09-25 08:24:43 -0500

    services

---
+++ asivamosffie.services/AppSettings.cs
@@ -0,0 +1,42 @@
+﻿namespace asivamosffie.services
+{
+    //internal class AppSettings
+    //{
+    //}
+
+    public class AppSettings
+    {
+        public string Dominio { get; set; }
+
+        public string DominioFront { get; set; }
+
+        public string MailServer { get; set; }
+
+        public int MailPort { get; set; }
+
+        public bool EnableSSL { get; set; }
+
+        public string SenderName { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Password { get; set; }
+
+        public string Secret { get; set; }
+
+        public string asivamosffieIssuerJwt { get; set; }
+
+        public string asivamosffieAudienceJwt { get; set; }
+
+        public string DirectoryBase { get; set; }
+
+        public string DirectoryBaseCargue { get; set; }
+
+        public string DirectoryBaseProyectos { get; set; }
+
+        public string YearVigente { get; set; }
+
+        public bool YearSiguienteEsVigente { get; set; }
+        public string DirectoryBaseOrdeELegibilidad { get; set; }
+    }
+}
