a2c693520c932fbe68056b2595349d3820b1de8c
xion1010011010 <mr.xion@hotmail.com>
2020-09-27 19:56:21 -0500

    Temporal

--- asivamosffie.services/Helpers/Constants/ConstanCodigoEstadoActaContrato.cs
+++ asivamosffie.services/Helpers/Constants/ConstanCodigoEstadoActaContrato.cs
@@ -3,8 +3,9 @@
     public static class ConstanCodigoEstadoActaContrato
     {
         public const string Sin_acta_generada = "1";
-        public const string Con_acta_generada = "2";
-        public const string Con_acta_en_proceso_de_firma = "3";
-        public const string Con_acta_suscrita_y_cargada = "4";
+        public const string Con_acta_preliminar_generada = "2";
+        public const string Con_acta_generada = "3";
+        public const string Con_acta_en_proceso_de_firma = "4";
+        public const string Con_acta_suscrita_y_cargada = "5";
     }
 }
