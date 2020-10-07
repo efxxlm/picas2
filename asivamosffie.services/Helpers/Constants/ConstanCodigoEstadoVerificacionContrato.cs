f51feb0486b0b0715fbc3733cd225758efc3aae2
xion1010011010 <mr.xion@hotmail.com>
2020-10-07 15:15:14 -0500

    tempo

--- asivamosffie.services/Helpers/Constants/ConstanCodigoEstadoVerificacionContrato.cs
+++ asivamosffie.services/Helpers/Constants/ConstanCodigoEstadoVerificacionContrato.cs
@@ -1,23 +1,21 @@
 ﻿namespace asivamosffie.services.Helpers.Constant
 {
-    public static class ConstanCodigoEstadoVerificacionContrato
+    public static class ConstanCodigoEstadoVerificacionContratoObra
     {
-
-        public const string Sin_aprobacion_de_requisitos_tecnicos = "1";
-        public const string En_proceso_de_aprobación_de_requisitos_tecnicos="2";
-        public const string Con_requisitos_tecnicos_aprobados= "3";
-        public const string Enviado_por_el_supervisor = "4";
-
-
-        //Sin_verificación_de_requisitos
-        //Requisitos_del_contratista_de_obra_en_proceso_de_verificación
-        //Con_requisitos_del_contratista_de_obra_verificados
-        //Con_requisitos_del_contratista_de_obra_avalados
-        //Con_notificación_al_supervisor
-        //Sin_revisión_de_requisitos_técnicos
-        //En_proceso_de_revisión_de_requisitos_técnicos
-        //Con_requisitos_técnicos_revisados
-        //Con_notificación_al_interventor
-        //Con_requisitos_técnico_aprobados
+        //Estados Verificacion contrato tipo obra
+        public const string Sin_verificacion_de_requisitos = "1";
+        public const string Requisitos_del_contratista_de_obra_en_proceso_de_verificacion = "2";
+        public const string Con_requisitos_del_contratista_de_obra_verificados = "3";
+        public const string Con_requisitos_del_contratista_de_obra_avalados = "4";
+        public const string Con_notificacion_al_supervisor = "5";
+    }
+    public static class ConstanCodigoEstadoVerificacionContratoInterventoria
+    {
+        //Estados Verificacion contrato tipo interventoria
+        public const string Sin_revision_de_requisitos_tecnicos = "6";
+        public const string En_proceso_de_revision_de_requisitos_tecnicos = "7";
+        public const string Con_requisitos_tecnicos_revisados = "8";
+        public const string Con_notificacion_al_interventor = "9";
+        public const string Con_requisitos_tecnico_aprobados = "10";
     }
 }
