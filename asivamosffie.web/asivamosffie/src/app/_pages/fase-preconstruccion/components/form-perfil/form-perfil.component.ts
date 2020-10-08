4aef7bda530b15f608b67d162055d0b6d2703b5b
charlyg360ve <charlyg360ve@gmail.com>
2020-10-08 15:14:13 -0500

    fix-perfiles

--- asivamosffie.web/asivamosffie/src/app/_pages/fase-preconstruccion/components/form-perfil/form-perfil.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/fase-preconstruccion/components/form-perfil/form-perfil.component.ts
@@ -47,6 +47,14 @@ export class FormPerfilComponent implements OnInit {
     return this.formContratista.get( 'perfiles' ) as FormArray;
   };
 
+  disabledDate ( cantidadHvAprobadas: string, cantidadHvRequeridas: string, index: number ) {
+    if ( cantidadHvAprobadas >= cantidadHvRequeridas ) {
+      this.perfiles.controls[index].get( 'fechaAprobacion' ).enable();
+    } else {
+      this.perfiles.controls[index].get( 'fechaAprobacion' ).disable();
+    }
+  }
+
   perfilesProyecto () {
     if ( this.perfilProyecto.length === 0 ) {
       this.formContratista.get( 'numeroPerfiles' ).valueChanges
