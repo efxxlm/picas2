140b5d5ce8904f099a2beb0b31f7490ab6dc0dcb
David Benitez <divist100@gmail.com>
2020-10-13 12:01:28 -0500

    ajustes

--- asivamosffie.services/Helpers/Enumerator/EnumeratorTemplate.cs
+++ asivamosffie.services/Helpers/Enumerator/EnumeratorTemplate.cs
@@ -4,16 +4,5 @@ namespace asivamosffie.services.Helpers.Enumerator
     public enum enumeratorTemplate
     {
          RecuperarClave = 2,
- 
-         DisponibilidadPresupuestalGenerada=4,
-         MsjSupervisorGestionPoliza=5,
-	MsjFiduciariaJuridicaGestionPoliza=6,
-	MsjInterventorGestionPoliza=7,
-	MsjSupervisorJuridicaGestionPoliza=8,
-        SolicitarApertura=10,
-        proyectoadministrativocreado=12
- 
-
- 
     }     
 }
