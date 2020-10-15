384ad40b31dd6f84c3cb29de4a61e5466211a8fc
David Benitez <divist100@gmail.com>
2020-10-15 10:00:42 -0500

    ajustes de maquetacion de la tabla

--- asivamosffie.web/asivamosffie/src/app/_pages/cargar-enlace-sistema-monitoreo-linea/components/acordion-tabla-lista-proyectos-cesml/acordion-tabla-lista-proyectos-cesml.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/cargar-enlace-sistema-monitoreo-linea/components/acordion-tabla-lista-proyectos-cesml/acordion-tabla-lista-proyectos-cesml.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-acordion-tabla-lista-proyectos-cesml',
@@ -6,10 +9,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acordion-tabla-lista-proyectos-cesml.component.scss']
 })
 export class AcordionTablaListaProyectosCesmlComponent implements OnInit {
-
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  displayedColumns: string[] = [
+    'llaveMen',
+    'tipoIntervencion',
+    'region',
+    'departamento',
+    'municipio',
+    'institucionEducativa',
+    'sede',
+    'gestion'
+  ];
+  dataTable: any[] = [
+    {
+      llaveMen: 'LJ776554',
+      tipoIntervencion: 'Remodelación',
+      region: 'Caribe',
+      departamento: 'Atlántico',
+      municipio: 'Malambo',
+      institucionEducativa: 'I.E. María Villa Campo',
+      sede: 'Única sede',
+      sitioWeb: '',
+      id: 1
+    },
+    {
+      llaveMen: 'LU990088',
+      tipoIntervencion: 'Remodelación',
+      region: 'Caribe',
+      departamento: 'Atlántico',
+      municipio: 'Baranoa',
+      institucionEducativa: 'María Inmaculada',
+      sede: 'Sede 2',
+      sitioWeb: '',
+      id: 2
+    },
+    {
+      llaveMen: 'LY665533',
+      tipoIntervencion: 'Remodelación',
+      region: 'Caribe',
+      departamento: 'Atlántico',
+      municipio: 'Soledad',
+      institucionEducativa: 'I.E. Primera de Mayo',
+      sede: 'Única sede',
+      sitioWeb: 'http://www.ffie.com',
+      id: 3
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.dataTable);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  };
 }
