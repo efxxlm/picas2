e071a99b861905bb60e76143799b666b94f67e1e
Johan Galeano <johans.galeanot@hotmail.com>
2020-10-22 11:56:36 -0500

    fix bugs 3.1.5

--- asivamosffie.web/asivamosffie/src/app/_pages/solicitar-contratacion/components/form-solicitar-contratacion/form-solicitar-contratacion.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/solicitar-contratacion/components/form-solicitar-contratacion/form-solicitar-contratacion.component.ts
@@ -42,26 +42,24 @@ export class FormSolicitarContratacionComponent implements OnInit {
   listaResultado: ProyectoGrilla[] = [];
 
   constructor(
-                private fb: FormBuilder,
-                private commonService: CommonService,
-                private projectService: ProjectService,
-                private dialog: MatDialog
-              ) 
-  {
+    private fb: FormBuilder,
+    private commonService: CommonService,
+    private projectService: ProjectService,
+    private dialog: MatDialog
+  ) {
     this.declararEsMultiple();
   }
 
   ngOnInit(): void {
-    
+
     forkJoin([
       this.commonService.listaTipoIntervencion(),
       this.commonService.listaRegion(),
-
-      //this.projectService..
-    ]).subscribe( response => {
+      // this.projectService..
+    ]).subscribe(response => {
       this.selectTipoInterventor = response[0];
-      this.selectRegion = response[1]
-    })
+      this.selectRegion = response[1];
+    });
   }
 
   openDialog(modalTitle: string, modalText: string) {
@@ -71,7 +69,7 @@ export class FormSolicitarContratacionComponent implements OnInit {
     });
   }
 
-  reiniciarFiltro () {
+  reiniciarFiltro() {
     this.addressForm.setValue({
       tipoInterventor: null,
       llaveMEN: null,
@@ -81,7 +79,7 @@ export class FormSolicitarContratacionComponent implements OnInit {
       institucionEducativa: null,
       sede: null
     });
-  };
+  }
 
   private declararEsMultiple() {
     this.esMultiple = new FormControl('free', Validators.required);
@@ -93,71 +91,68 @@ export class FormSolicitarContratacionComponent implements OnInit {
     return alphanumeric.test(inputChar) ? true : false;
   }
 
-  changeRegion(){
-    let region = this.addressForm.get('region').value;
-    this.commonService.listaDepartamentosByRegionId( region ? region.localizacionId.toString() : null )
-        .subscribe( listaDeptos => {
-          this.selectDepartamento = listaDeptos;
-        })
+  changeRegion() {
+    const region = this.addressForm.get('region').value;
+    this.commonService.listaDepartamentosByRegionId(region ? region.localizacionId.toString() : null)
+      .subscribe(listaDeptos => {
+        this.selectDepartamento = listaDeptos;
+      });
   }
 
-  changeDepartamento(){
-    let departamento = this.addressForm.get('departamento').value;
-    this.commonService.listaMunicipiosByIdDepartamento( departamento ? departamento.localizacionId.toString() : null )
-    .subscribe( listaMun => {
-      this.selectMunicipio = listaMun;
-    })
+  changeDepartamento() {
+    const departamento = this.addressForm.get('departamento').value;
+    this.commonService.listaMunicipiosByIdDepartamento(departamento ? departamento.localizacionId.toString() : null)
+      .subscribe(listaMun => {
+        this.selectMunicipio = listaMun;
+      });
   }
 
-  changeMunicipio(){
-    let municipio = this.addressForm.get('municipio').value;
-    this.commonService.listaIntitucionEducativaByMunicipioId( municipio ? municipio.localizacionId.toString() : null )
-    .subscribe( listaInst => {
-      this.selectinstitucionEducativa = listaInst;
-    })
+  changeMunicipio() {
+    const municipio = this.addressForm.get('municipio').value;
+    this.commonService.listaIntitucionEducativaByMunicipioId(municipio ? municipio.localizacionId.toString() : null)
+      .subscribe(listaInst => {
+        this.selectinstitucionEducativa = listaInst;
+      });
   }
 
-  changeInstitucionE(){
-    let instituto: InstitucionEducativa = this.addressForm.get('institucionEducativa').value;
-    console.log( instituto );
-    this.commonService.listaSedeByInstitucionEducativaId( instituto ? instituto.institucionEducativaSedeId : null )
-    .subscribe( listaSedes => {
-      this.selectSede = listaSedes;
-    })
+  changeInstitucionE() {
+    const instituto: InstitucionEducativa = this.addressForm.get('institucionEducativa').value;
+    console.log(instituto);
+    this.commonService.listaSedeByInstitucionEducativaId(instituto ? instituto.institucionEducativaSedeId : null)
+      .subscribe(listaSedes => {
+        this.selectSede = listaSedes;
+      });
   }
 
   onSubmit() {
-    
-    let pTipoIntervencion: Dominio = this.addressForm.get('tipoInterventor').value;
-    let pLlaveMen: string = this.addressForm.get('llaveMEN').value;
-    let pRegion: Localizacion = this.addressForm.get('region').value;
-    let pDepartamento: Localizacion = this.addressForm.get('departamento').value;
-    let pMunicipio: Localizacion = this.addressForm.get('municipio').value;
-    let pIdInstitucionEducativa: InstitucionEducativa = this.addressForm.get('institucionEducativa').value;
-    let pIdSede: InstitucionEducativa = this.addressForm.get('sede').value;
 
-    this.projectService.listaProyectoConFiltros( 
-                                                  pTipoIntervencion ? pTipoIntervencion.codigo : "", 
-                                                  pLlaveMen ? pLlaveMen : "", 
-                                                  pRegion ? pRegion.localizacionId == "7" ? "" : pRegion.localizacionId : "",
-                                                  pDepartamento ? pDepartamento.localizacionId : "",
-                                                  pMunicipio ? pMunicipio.localizacionId : "", 
-                                                  pIdInstitucionEducativa ? pIdInstitucionEducativa.institucionEducativaSedeId : 0, 
-                                                  pIdSede ? pIdSede.institucionEducativaSedeId : 0 
+    const pTipoIntervencion: Dominio = this.addressForm.get('tipoInterventor').value;
+    const pLlaveMen: string = this.addressForm.get('llaveMEN').value;
+    const pRegion: Localizacion = this.addressForm.get('region').value;
+    const pDepartamento: Localizacion = this.addressForm.get('departamento').value;
+    const pMunicipio: Localizacion = this.addressForm.get('municipio').value;
+    const pIdInstitucionEducativa: InstitucionEducativa = this.addressForm.get('institucionEducativa').value;
+    const pIdSede: InstitucionEducativa = this.addressForm.get('sede').value;
 
-                                               ).subscribe( proyectos => {
-                                                  this.listaResultado = proyectos;
-                                                  if ( proyectos.length === 0 ) {
-                                                    this.openDialog( '', 'No se encontraron registros asociados a los criterios de búsqueda.' );
-                                                    return;
-                                                  };
-                                                  setTimeout(() => { 
-                                                                      let btnListaResultado = document.getElementById('btnListaResultado');
-                                                                      btnListaResultado.click(); 
-                                                                    },1000)
+    this.projectService.listaProyectoConFiltros(
+      pTipoIntervencion ? pTipoIntervencion.codigo : '',
+      pLlaveMen ? pLlaveMen : '',
+      pRegion ? pRegion.localizacionId === '7' ? '' : pRegion.localizacionId : '',
+      pDepartamento ? pDepartamento.localizacionId : '',
+      pMunicipio ? pMunicipio.localizacionId : '',
+      pIdInstitucionEducativa ? pIdInstitucionEducativa.institucionEducativaSedeId : 0,
+      pIdSede ? pIdSede.institucionEducativaSedeId : 0
 
-                                                  
-
-                                               })
+    ).subscribe(proyectos => {
+      this.listaResultado = proyectos;
+      if (proyectos.length === 0) {
+        this.openDialog('', '<b>No se encontraron registros asociados a los criterios de búsqueda.</b>');
+        return;
+      }
+      setTimeout(() => {
+        const btnListaResultado = document.getElementById('btnListaResultado');
+        btnListaResultado.click();
+      }, 1000);
+    });
   }
 }
