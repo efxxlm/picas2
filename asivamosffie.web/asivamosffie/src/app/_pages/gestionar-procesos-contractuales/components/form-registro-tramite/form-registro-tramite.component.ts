93013fcf68ff5c5c6bcb798ffb6cc768a3b215fe
Johan Galeano <johans.galeanot@hotmail.com>
2020-11-09 09:35:20 -0500

    ayuda hover

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-procesos-contractuales/components/form-registro-tramite/form-registro-tramite.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-procesos-contractuales/components/form-registro-tramite/form-registro-tramite.component.ts
@@ -15,10 +15,11 @@ export class FormRegistroTramiteComponent implements OnInit, OnDestroy {
   fechaSesionString: string;
   fechaSesion: Date;
   @Input() minDate: Date;
-  archivo                : string;
+  archivo: string;
   @Input() dataFormulario: FormGroup;
-  @Input() contratacion  : DataSolicitud;
-  seRealizoPeticion: boolean = false;
+  @Input() contratacion: DataSolicitud;
+  seRealizoPeticion = false;
+  camposFaltantes = false;
   editorStyle = {
     height: '45px'
   };
@@ -31,20 +32,19 @@ export class FormRegistroTramiteComponent implements OnInit, OnDestroy {
     ]
   };
 
-  constructor ( private dialog: MatDialog,
-                private procesosContractualesSvc: ProcesosContractualesService,
-                private routes: Router ) 
-  {
+  constructor(private dialog: MatDialog,
+              private procesosContractualesSvc: ProcesosContractualesService,
+              private routes: Router) {
   }
 
   ngOnDestroy(): void {
-    if ( this.dataFormulario.dirty === true && this.seRealizoPeticion === false ) {
-      this.openDialogConfirmar( '', '¿Desea guardar la información registrada?' );
-    };
-  };
+    if (this.dataFormulario.dirty === true && this.seRealizoPeticion === false) {
+      this.openDialogConfirmar('', '¿Desea guardar la información registrada?');
+    }
+  }
 
   ngOnInit(): void {
-    this.archivo = this.dataFormulario.get( 'minutaName' ).value;
+    this.archivo = this.dataFormulario.get('minutaName').value;
   }
 
   openDialogConfirmar(modalTitle: string, modalText: string) {
@@ -54,90 +54,95 @@ export class FormRegistroTramiteComponent implements OnInit, OnDestroy {
     });
 
     confirmarDialog.afterClosed()
-      .subscribe( response => {
-        if ( response === true ) {
+      .subscribe(response => {
+        if (response === true) {
           this.guardar();
         }
-      } );
-  };
+      });
+  }
 
-  fileName ( event: any ) {
-    
-    if ( event.target.files.length > 0) {
-      const file   = event.target.files[0];
+  fileName(event: any) {
+
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
       this.archivo = event.target.files[0].name;
       this.dataFormulario.patchValue({
         minutaFile: file
       });
-    };
-
-  };
-
-  textoLimpio (texto: string) {
-    if ( texto ){
-      const textolimpio = texto.replace(/<[^>]*>/g, '');
-      return textolimpio.length;
-    };
-  };
-
-  maxLength (e: any, n: number) {
-    if (e.editor.getLength() > n) {
-      e.editor.deleteText(n, e.editor.getLength());
-    };
-  };
-
-  openDialog (modalTitle: string, modalText: string) {
-    this.dialog.open(ModalDialogComponent, {
-      width: '28em',
-      data : { modalTitle, modalText }
-    });
-  };
-
-  guardar () {
-    console.log( this.dataFormulario );
-
-    this.contratacion.observaciones = this.dataFormulario.get( 'observaciones' ).value;
-
-    let fecha = Date.parse(this.dataFormulario.get( 'fechaEnvioTramite' ).value);
-    this.fechaSesion = new Date(fecha);
-    this.fechaSesionString = `${ this.fechaSesion.getFullYear() }/${ this.fechaSesion.getMonth() + 1 }/${ this.fechaSesion.getDate() }` 
-    this.contratacion.fechaEnvioDocumentacion =   this.fechaSesionString 
-    let documento: any = document.getElementById('file');
-    if ( this.dataFormulario.get( 'minutaFile' ).value.size > 1048576 ) {
-      this.openDialog( '', '<b>El tamaño del archivo es superior al permitido, debe subir un archivo máximo de 1MB.</b>' );
-      return;
-    };
-    if ( documento !== null ) {
-      let pFile = this.dataFormulario.get( 'minutaFile' ).value;
-      pFile = pFile.name.split( '.' )
-      pFile = pFile[ pFile.length -1 ]
-      console.log( pFile );
-      if ( pFile === 'docx' || pFile === 'doc' ) {
-        this.procesosContractualesSvc.sendTramite( this.contratacion, documento.files[0] )
-        .subscribe( 
-          response => {
-            this.seRealizoPeticion = true;
-            this.openDialog( '' , response.message );
-            this.routes.navigate( [ '/procesosContractuales' ] );
-          },
-          err => this.openDialog( '' , err.message )
-        ); 
-      } else {
-        this.openDialog( '', '<b>El tipo de archivo que esta intentando cargar no es permitido en la plataforma.<br>El tipo de documento soportado es .doc y .docx</b>' );
-        return;
-      }
-    } else {
-      this.procesosContractualesSvc.sendTramite( this.contratacion, undefined )
-      .subscribe( 
-        response => {
-          this.seRealizoPeticion = true;
-          this.openDialog( '' , response.message );
-          this.routes.navigate( [ '/procesosContractuales' ] );
-        },
-        err => this.openDialog( '' , err.message )
-      );
     }
 
-  };
+  }
 
-};
+  textoLimpio(texto: string) {
+    if (texto) {
+      const textolimpio = texto.replace(/<[^>]*>/g, '');
+      return textolimpio.length;
+    }
+  }
+
+  maxLength(e: any, n: number) {
+    if (e.editor.getLength() > n) {
+      e.editor.deleteText(n, e.editor.getLength());
+    }
+  }
+
+  openDialog(modalTitle: string, modalText: string) {
+    this.dialog.open(ModalDialogComponent, {
+      width: '28em',
+      data: { modalTitle, modalText }
+    });
+  }
+
+  guardar(): void {
+    if (this.dataFormulario.valid) {
+      this.camposFaltantes = false;
+
+      this.contratacion.observaciones = this.dataFormulario.get('observaciones').value;
+
+      const fecha = Date.parse(this.dataFormulario.get('fechaEnvioTramite').value);
+      this.fechaSesion = new Date(fecha);
+      this.fechaSesionString = `${this.fechaSesion.getFullYear()}/${this.fechaSesion.getMonth() + 1}/${this.fechaSesion.getDate()}`;
+      this.contratacion.fechaEnvioDocumentacion = this.fechaSesionString;
+      const documento: any = document.getElementById('file');
+      if (this.dataFormulario.get('minutaFile').value.size > 1048576) {
+        this.openDialog('', '<b>El tamaño del archivo es superior al permitido, debe subir un archivo máximo de 1MB.</b>');
+        return;
+      }
+      if (documento !== null) {
+        let pFile = this.dataFormulario.get('minutaFile').value;
+        pFile = pFile.name.split('.');
+        pFile = pFile[pFile.length - 1];
+        console.log(pFile);
+        if (pFile === 'docx' || pFile === 'doc') {
+          this.procesosContractualesSvc.sendTramite(this.contratacion, documento.files[0])
+            .subscribe(
+              response => {
+                this.seRealizoPeticion = true;
+                this.openDialog('', response.message);
+                this.routes.navigate(['/procesosContractuales']);
+              },
+              err => this.openDialog('', err.message)
+            );
+        } else {
+          this.openDialog('', '<b>El tipo de archivo que esta intentando cargar no es permitido en la plataforma.<br>El tipo de documento soportado es .doc y .docx</b>');
+          return;
+        }
+      } else {
+        this.procesosContractualesSvc.sendTramite(this.contratacion, undefined)
+          .subscribe(
+            response => {
+              this.seRealizoPeticion = true;
+              this.openDialog('', response.message);
+              this.routes.navigate(['/procesosContractuales']);
+            },
+            err => this.openDialog('', err.message)
+          );
+      }
+
+    }
+    else {
+      this.camposFaltantes = true;
+    }
+  }
+
+}
