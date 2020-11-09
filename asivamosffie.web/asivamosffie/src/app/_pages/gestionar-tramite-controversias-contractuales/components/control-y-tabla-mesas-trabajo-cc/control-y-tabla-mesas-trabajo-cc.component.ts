38ab2b4b6925c8c7ea452f5b33b9cdf5d96a6511
David Benitez <divist100@gmail.com>
2020-11-09 15:56:44 -0500

    fix front 4.2.1

---
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-tramite-controversias-contractuales/components/control-y-tabla-mesas-trabajo-cc/control-y-tabla-mesas-trabajo-cc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-y-tabla-mesas-trabajo-cc',
+  templateUrl: './control-y-tabla-mesas-trabajo-cc.component.html',
+  styleUrls: ['./control-y-tabla-mesas-trabajo-cc.component.scss']
+})
+export class ControlYTablaMesasTrabajoCcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
