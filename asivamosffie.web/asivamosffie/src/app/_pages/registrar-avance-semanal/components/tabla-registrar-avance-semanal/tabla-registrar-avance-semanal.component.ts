a8f4078360fd6187a52dd065016869819ed1a660
Carlos Molero <61335101+CharlyG360VE@users.noreply.github.com>
2020-11-09 15:47:22 -0500

    front-4.1.12-v1

---
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-avance-semanal/components/tabla-registrar-avance-semanal/tabla-registrar-avance-semanal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-registrar-avance-semanal',
+  templateUrl: './tabla-registrar-avance-semanal.component.html',
+  styleUrls: ['./tabla-registrar-avance-semanal.component.scss']
+})
+export class TablaRegistrarAvanceSemanalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
