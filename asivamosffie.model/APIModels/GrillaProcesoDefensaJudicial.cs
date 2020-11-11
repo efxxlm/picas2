39fb667230e14ffb4fe8fd7e57b762022426ac14
Camilo Daza <antirbd_11@hotmail.com>
2020-11-11 13:58:42 -0600

    ListGrillaProcesosDefensaJudicial 4.2.2

---
+++ asivamosffie.model/APIModels/GrillaProcesoDefensaJudicial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asivamosffie.model.APIModels
+{
+   public class GrillaProcesoDefensaJudicial
+    {        
+        public int DefensaJudicialId { get; set; }
+        public string FechaRegistro { get; set; }
+        public string LegitimacionPasivaActiva { get; set; }
+        public string NumeroProceso { get; set; }
+        public string TipoAccion { get; set; }
+        public string TipoAccionCodigo { get; set; }
+        public string EstadoProceso { get; set; }
+        public string EstadoProcesoCodigo { get; set; }
+        //public string RegistroCompletoCodigo { get; set; }
+        public string RegistroCompletoNombre { get; set; }
+        public string TipoProceso { get; set; }
+        public string TipoProcesoCodigo { get; set; }
+
+
+    }
+}
---
+++ asivamosffie.services/JudicialDefenseService.cs
@@ -0,0 +1,120 @@
+﻿using asivamosffie.model.APIModels;
+using asivamosffie.model.Models;
+using asivamosffie.services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using asivamosffie.services.Helpers.Enumerator;
+
+namespace asivamosffie.services
+{
+    public class JudicialDefenseService /*: IGuaranteePolicyService*/
+    {
+        private readonly ICommonService _commonService;
+        private readonly devAsiVamosFFIEContext _context;
+
+        public JudicialDefenseService(devAsiVamosFFIEContext context, ICommonService commonService)
+        {
+
+            _commonService = commonService;
+            _context = context;
+            //_settings = settings;
+        }
+        public async Task<List<GrillaProcesoDefensaJudicial>> ListGrillaProcesosDefensaJudicial()
+        {
+            //await AprobarContratoByIdContrato(1);
+
+            List<GrillaProcesoDefensaJudicial> ListDefensaJudicialGrilla = new List<GrillaProcesoDefensaJudicial>();
+            //Fecha de firma del contrato ??? FechaFirmaContrato , [Contrato] -(dd / mm / aaaa)
+
+            //Tipo de solicitud ??? ContratoPoliza - TipoSolicitudCodigo        
+
+            //List <Contrato> ListContratos = await _context.Contrato.Where(r => !(bool)r.Estado).Include(r => r.FechaFirmaContrato).Include(r => r.NumeroContrato).Include(r => r.Estado).Distinct().ToListAsync();
+            List<DefensaJudicial> ListDefensaJudicial  = await _context.DefensaJudicial.Where(r => (bool)r.Eliminado==false).Distinct().ToListAsync();
+
+            foreach (var defensaJudicial in ListDefensaJudicial)
+            {
+                try
+                {     
+                    //tiposol contratoPoliza = await _commonService.GetContratoPolizaByContratoId(contrato.ContratoId);
+                    string strTipoSolicitudCodigoContratoPoliza = "sin definir";
+                    string strEstadoSolicitudCodigoContratoPoliza = "sin definir";
+
+                    //Localizacion departamento = await _commonService.GetDepartamentoByIdMunicipio(proyecto.LocalizacionIdMunicipio);
+                    Dominio TipoSolicitudCodigoContratoPoliza;
+                    Dominio EstadoSolicitudCodigoContratoPoliza;
+
+                    //if (contratoPoliza != null)
+                    //{
+                        //TipoSolicitudCodigoContratoPoliza = await _commonService.GetDominioByNombreDominioAndTipoDominio(contratoPoliza.TipoSolicitudCodigo, (int)EnumeratorTipoDominio.Tipo_Modificacion_Contrato_Poliza);
+                        //TipoSolicitudCodigoContratoPoliza = await _commonService.GetDominioByNombreDominioAndTipoDominio(contratoPoliza.TipoSolicitudCodigo.Trim(), (int)EnumeratorTipoDominio.Tipo_de_Solicitud);
+                        //if (TipoSolicitudCodigoContratoPoliza != null)
+                        //    strTipoSolicitudCodigoContratoPoliza = TipoSolicitudCodigoContratoPoliza.Nombre;
+
+                        //EstadoSolicitudCodigoContratoPoliza = await _commonService.GetDominioByNombreDominioAndTipoDominio(contratoPoliza.EstadoPolizaCodigo.Trim(), (int)EnumeratorTipoDominio.Estado_Contrato_Poliza);
+                        //if (EstadoSolicitudCodigoContratoPoliza != null)
+                        //    strEstadoSolicitudCodigoContratoPoliza = EstadoSolicitudCodigoContratoPoliza.Nombre;
+
+                    //}
+                    bool bRegistroCompleto = false;
+                    string strRegistroCompleto = "Incompleto";
+
+                    //if (defensaJudicial.EsCompleto != null)
+                    //{
+                        strRegistroCompleto = (bool)defensaJudicial.EsCompleto ? "Completo" : "Incompleto";
+                    //}
+
+                    //Dominio EstadoSolicitudCodigoContratoPoliza = await _commonService.GetDominioByNombreDominioAndTipoDominio(contratoPoliza.TipoSolicitudCodigo, (int)EnumeratorTipoDominio.Estado_Contrato_Poliza);
+                    GrillaProcesoDefensaJudicial defensaJudicialGrilla = new GrillaProcesoDefensaJudicial
+                    {
+                        DefensaJudicialId = defensaJudicial.DefensaJudicialId,
+                        FechaRegistro = defensaJudicial.FechaCreacion.ToString("dd/MM/yyyy"),
+                        LegitimacionPasivaActiva= (bool)defensaJudicial.EsLegitimacionActiva ? "Activa" : "Pasiva",
+                        NumeroProceso="DJ"+ defensaJudicial.DefensaJudicialId.ToString() + defensaJudicial.FechaCreacion.ToString("yyyy"),
+                        TipoAccionCodigo= defensaJudicial.TipoAccionCodigo,
+                        TipoAccion = "PENDIENTE",
+                        EstadoProceso= "PENDIENTE",
+                        EstadoProcesoCodigo =defensaJudicial.EstadoProcesoCodigo,
+                        
+                        RegistroCompletoNombre =strRegistroCompleto,
+                        TipoProceso="PENDIENTE",
+                        TipoProcesoCodigo = defensaJudicial.TipoProcesoCodigo,                       
+                                          
+                    };
+
+                    //if (!(bool)proyecto.RegistroCompleto)
+                    //{
+                    //    proyectoGrilla.EstadoRegistro = "INCOMPLETO";
+                    //}
+                    ListDefensaJudicialGrilla.Add(defensaJudicialGrilla);
+                }
+                catch (Exception e)
+                {
+                    GrillaProcesoDefensaJudicial defensaJudicialGrilla = new GrillaProcesoDefensaJudicial
+                    {
+                        DefensaJudicialId = defensaJudicial.DefensaJudicialId,
+                        FechaRegistro = e.ToString(),
+                        LegitimacionPasivaActiva = e.InnerException.ToString(),
+                        NumeroProceso = "ERROR",
+                        TipoAccionCodigo = "ERROR",
+                        TipoAccion = "ERROR",
+                        EstadoProceso = "ERROR",
+                        EstadoProcesoCodigo = "ERROR",
+
+                        RegistroCompletoNombre = "ERROR",
+                        TipoProceso = "ERROR",
+                        TipoProcesoCodigo = "ERROR",                  
+    
+                    };
+                    ListDefensaJudicialGrilla.Add(defensaJudicialGrilla);
+                }
+            }
+            return ListDefensaJudicialGrilla.ToList();
+
+        }
+    }
+}
