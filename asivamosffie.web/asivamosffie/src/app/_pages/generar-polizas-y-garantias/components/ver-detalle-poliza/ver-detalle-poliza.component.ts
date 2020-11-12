37c79f3bb512be9f662ac36195bc47711d52e485
David Benitez <divist100@gmail.com>
2020-11-12 14:25:27 -0500

    ajustes de ver detalle 3.4.4

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-polizas-y-garantias/components/ver-detalle-poliza/ver-detalle-poliza.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-polizas-y-garantias/components/ver-detalle-poliza/ver-detalle-poliza.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PolizaGarantiaService } from 'src/app/core/_services/polizaGarantia/poliza-garantia.service';
 
 @Component({
   selector: 'app-ver-detalle-poliza',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-detalle-poliza.component.scss']
 })
 export class VerDetallePolizaComponent implements OnInit {
+  fechaFirmaContrato: any;
+  tipoSolicitud: any;
+  tipoContrato: any;
+  objeto: any;
+  nombreContratista: any;
+  tipoIdentificacion: any;
+  numeroIdentificacion: any;
+  valorContrato: any;
+  plazoContrato: any;
+  numContrato: any;
+  nomAseguradora: any;
+  numPoliza: any;
+  numCertificado: any;
+  fechaExpPoliza: any;
+  vigenciaPoliza: any;
+  vigenciaAmparo: any;
+  valorAmparo: number;
 
-  constructor() { }
+  constructor(    
+    private polizaService: PolizaGarantiaService,
+    private activatedRoute: ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe( param => {
+      this.cargarDatosGenerales(param.id);
+    });
   }
-
+  cargarDatosGenerales(id){
+    this.polizaService.GetListVistaContratoGarantiaPoliza(id).subscribe(data => {
+      this.fechaFirmaContrato = data[0].fechaFirmaContrato;
+      this.tipoSolicitud = data[0].tipoSolicitud;
+      this.tipoContrato = data[0].tipoContrato;
+      this.objeto = data[0].descripcionModificacion;
+      this.nombreContratista = data[0].nombreContratista;
+      this.tipoIdentificacion = data[0].tipoDocumento;
+      this.numeroIdentificacion = data[0].numeroIdentificacion;
+      this.valorContrato = data[0].valorContrato;
+      this.plazoContrato = data[0].plazoContrato;
+      this.numContrato = data[0].numeroContrato;
+    });
+    this.polizaService.GetContratoPolizaByIdContratoId(id).subscribe(data0=>{
+      this.nomAseguradora = data0.nombreAseguradora;
+      this.numPoliza = data0.numeroPoliza;
+      this.numCertificado = data0.numeroCertificado;
+      this.fechaExpPoliza = data0.fechaExpedicion;
+      this.vigenciaPoliza = data0.vigencia;
+      this.vigenciaAmparo = data0.vigenciaAmparo;
+      this.valorAmparo = data0.valorAmparo;
+    });
+  }
 }
