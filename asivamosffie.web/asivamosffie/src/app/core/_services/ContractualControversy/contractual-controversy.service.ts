c811e6dfbf41d8d30bccda32c512c319dc22ba03
David Benitez <divist100@gmail.com>
2020-11-17 09:31:40 -0500

    integracion 4.2.1

--- asivamosffie.web/asivamosffie/src/app/core/_services/ContractualControversy/contractual-controversy.service.ts
+++ asivamosffie.web/asivamosffie/src/app/core/_services/ContractualControversy/contractual-controversy.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Respuesta } from '../autenticacion/autenticacion.service';
+import { environment } from 'src/environments/environment';
+import { pid } from 'process';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ContractualControversyService {
+export class ContractualControversyService implements OnInit{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+
+  }
+  CreateEditarControversiaTAI(controversiaContractual: any){
+    return this.http.post<Respuesta>(`${environment.apiUrl}/ContractualControversy/CreateEditarControversiaTAI`, controversiaContractual);
+  }
+  CreateEditNuevaActualizacionTramite(controversiaActuacion: any){
+    return this.http.post<Respuesta>(`${environment.apiUrl}/ContractualControversy/CreateEditNuevaActualizacionTramite`, controversiaActuacion);
+  }
+  GetListGrillaTipoSolicitudControversiaContractual(){
+    return this.http.get<GetListGrillaTipoSolicitudControversiaContractual>(`${environment.apiUrl}/ContractualControversy/GetListGrillaTipoSolicitudControversiaContractual`);
+  }
+  GetVistaContratoContratista(pContratoId: number){
+    return this.http.get<GetVistaContratoContratista>(`${environment.apiUrl}/ContractualControversy/GetVistaContratoContratista?pContratoId=${pContratoId}`);
+  }
+  CambiarEstadoControversiaContractual(pControversiaContractualId:number, pNuevoCodigoEstado:string){
+    
+  }
+}
+
+export interface GetListGrillaTipoSolicitudControversiaContractual{
+
+}
+
+export interface GetVistaContratoContratista{
+
 }
