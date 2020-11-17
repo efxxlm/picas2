525ba5f4de1e41c3f2cd40adb95f3aa5f4dcaf77
Julian Martinez <julian1010011010@gmail.com>
2020-11-17 15:23:29 -0500

    se vuelve a poner contratoOld a contrato  que se perdio en algun merge

--- asivamosffie.services/RegisterContractsAndContractualModificationsService.cs
+++ asivamosffie.services/RegisterContractsAndContractualModificationsService.cs
@@ -31,7 +31,7 @@ namespace asivamosffie.services
         public async Task<List<SesionComiteSolicitud>> GetListSesionComiteSolicitud()
         {
             List<SesionComiteSolicitud> ListSesionComiteSolicitud = await _context.SesionComiteSolicitud
-                .Where(r => !(bool)r.Eliminado
+                .Where(r => !(bool)r.Eliminado 
                    &&
                   ((r.TipoSolicitudCodigo == ConstanCodigoTipoSolicitud.Contratacion)
                    || r.TipoSolicitudCodigo == ConstanCodigoTipoSolicitud.Modificacion_Contractual
@@ -244,7 +244,7 @@ namespace asivamosffie.services
 
                 //Cambio pedido por yuly que se envia cuando
                 if (pEstadoCodigo == ConstanCodigoEstadoSolicitudContratacion.Registrados)
-                    await EnviarNotificaciones(pContrato, pDominioFront, pMailServer, pMailPort, pEnableSSL, pPassword, pSender);
+                    await EnviarNotificaciones(contratoOld, pDominioFront, pMailServer, pMailPort, pEnableSSL, pPassword, pSender);
 
 
                 //Save Files  
@@ -319,8 +319,17 @@ namespace asivamosffie.services
         public async Task<bool> EnviarNotificaciones(Contrato pContrato, string pDominioFront, string pMailServer, int pMailPort, bool pEnableSSL, string pPassword, string pSender)
         {
             Template TemplateRecoveryPassword = await _commonService.GetTemplateById((int)enumeratorTemplate.NotificacionContratacion341);
-            DateTime? FechaFirmaFiduciaria = _context.SesionComiteSolicitud.Where(r => r.SolicitudId == pContrato.Contratacion.ContratacionId && r.TipoSolicitudCodigo == ConstanCodigoTipoSolicitud.Contratacion).Select(r => r.ComiteTecnicoFiduciario.FechaOrdenDia).FirstOrDefault();
+            DateTime? FechaFirmaFiduciaria = null;
 
+            try
+            {
+                FechaFirmaFiduciaria = _context.SesionComiteSolicitud.Where(r => r.SolicitudId == pContrato.Contratacion.ContratacionId && r.TipoSolicitudCodigo == ConstanCodigoTipoSolicitud.Contratacion).Select(r => r.ComiteTecnicoFiduciario.FechaOrdenDia).FirstOrDefault();
+
+            }
+            catch (Exception)
+            { 
+            }
+            
             var emails = _context.UsuarioPerfil
                 .Where(x => (x.PerfilId == (int)EnumeratorPerfil.Juridica
                     || x.PerfilId == (int)EnumeratorPerfil.Tecnica) && x.Activo)
