b3aea3acf3118a454257899fb11b8683ebe5900a
Julian Martinez <julian1010011010@gmail.com>
2020-11-23 17:59:03 -0500

    Update Context

---
+++ asivamosffie.model/Models/SeguimientoSemanalPersonalObra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asivamosffie.model.Models
+{
+    public partial class SeguimientoSemanalPersonalObra
+    {
+        public int SeguimientoSemanalPersonalObraId { get; set; }
+        public int SeguimientoSemanalId { get; set; }
+        public int? CantidadPersonal { get; set; }
+        public bool Eliminado { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+
+        public virtual SeguimientoSemanal SeguimientoSemanal { get; set; }
+    }
+}
