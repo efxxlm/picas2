e9b5566a8dd7e31fa9239afa8abe558d91ba6d5c
Carlos Molero <61335101+CharlyG360VE@users.noreply.github.com>
2020-11-25 17:39:56 -0500

    fix-integracion-3.1.7-v2

--- asivamosffie.web/asivamosffie/src/app/_pages/verificar-preconstruccion/components/ver-detalle/ver-detalle.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/verificar-preconstruccion/components/ver-detalle/ver-detalle.component.ts
@@ -15,42 +15,43 @@ export class VerDetalleComponent implements OnInit {
   contrato: Contrato;
   perfilesCv: Dominio[] = [];
 
-  constructor ( private activatedRoute: ActivatedRoute,
-                private commonSvc: CommonService,
-                private faseUnoPreconstruccionSvc: FaseUnoPreconstruccionService ) 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private commonSvc: CommonService,
+    private faseUnoPreconstruccionSvc: FaseUnoPreconstruccionService )
   {
     this.getContratacionByContratoId( this.activatedRoute.snapshot.params.id );
     this.commonSvc.listaPerfil()
       .subscribe(
         response => this.perfilesCv = response
       );
-  };
+  }
 
   ngOnInit(): void {
-  };
+  }
 
-  getContratacionByContratoId ( pContratoId: string ) {
+  getContratacionByContratoId( pContratoId: string ) {
     this.faseUnoPreconstruccionSvc.getContratacionByContratoId( pContratoId )
       .subscribe( contrato => {
         this.contrato = contrato;
         console.log( this.contrato );
       } );
-  };
+  }
 
-  tipoPerfil ( perfilCodigo: string ) {
+  tipoPerfil( perfilCodigo: string ) {
     let perfilSeleccionado;
     if ( this.perfilesCv.length > 0 ) {
       perfilSeleccionado = this.perfilesCv.filter( value => value.codigo === perfilCodigo );
       perfilSeleccionado = perfilSeleccionado[0].nombre;
       return perfilSeleccionado;
-    };
-  };
+    }
+  }
 
-  innerObservacion ( observacion: string ) {
+  innerObservacion( observacion: string ) {
     if ( observacion !== undefined ) {
       const observacionHtml = observacion.replace( '"', '' );
       return observacionHtml;
-    };
-  };
+    }
+  }
 
-};
+}
