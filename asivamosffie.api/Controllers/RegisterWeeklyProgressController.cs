4cb7c2e6a2199fabe4e9d4c8ce15d0b6e5dc5073
Julian Martinez <julian1010011010@gmail.com>
2020-11-27 00:38:11 -0500

    Ctr Interface Service RegisterWeeklyProgress

---
+++ asivamosffie.api/Controllers/RegisterWeeklyProgressController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using asivamosffie.model.APIModels;
+using asivamosffie.model.Models;
+using asivamosffie.services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using asivamosffie.model.APIModels;
+
+namespace asivamosffie.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterWeeklyProgressController : Controller
+    {
+
+        private readonly IRegisterWeeklyProgressService _registerWeeklyProgressService;
+        private readonly IOptions<AppSettings> _settings;
+
+        public RegisterWeeklyProgressController(IRegisterWeeklyProgressService registerWeeklyProgressService, IOptions<AppSettings> settings)
+        {
+            _registerWeeklyProgressService = registerWeeklyProgressService;
+            _settings = settings;
+        }
+
+    }
+}
---
+++ asivamosffie.services/RegisterWeeklyProgressService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using asivamosffie.model.Models;
+using asivamosffie.services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using asivamosffie.services.Helpers.Constant;
+using asivamosffie.services.Helpers.Enumerator;
+using asivamosffie.model.APIModels;
+using System.IO;
+using Z.EntityFramework.Plus;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace asivamosffie.services
+{
+    public class RegisterWeeklyProgressService : IRegisterWeeklyProgressService
+    {
+        
+
+    }
+}
