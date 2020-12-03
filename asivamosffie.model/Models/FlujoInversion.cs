e00c9f9e505caa8b6bbb02cd99f199a2ed63739d
DiegoEstupinan <daestupinans@dane.gov.co>
2020-12-02 23:59:29 -0500

    flujo inversion

--- asivamosffie.model/Models/FlujoInversion.cs
+++ asivamosffie.model/Models/FlujoInversion.cs
@@ -7,12 +7,11 @@ namespace asivamosffie.model.Models
     {
         public int FlujoInversionId { get; set; }
         public int ContratoConstruccionId { get; set; }
-        public string Capitulo { get; set; }
         public string Semana { get; set; }
         public decimal? Valor { get; set; }
         public int? MesEjecucionId { get; set; }
         public int? ProgramacionId { get; set; }
-        
+
         public virtual ContratoConstruccion ContratoConstruccion { get; set; }
         public virtual MesEjecucion MesEjecucion { get; set; }
         public virtual Programacion Programacion { get; set; }
