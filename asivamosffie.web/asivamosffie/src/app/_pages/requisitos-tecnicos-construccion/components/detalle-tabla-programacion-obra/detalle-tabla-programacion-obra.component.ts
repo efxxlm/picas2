93a22e9c2e75d348c21628b28abd2653c5ad9ec9
DiegoEstupinan <daestupinans@dane.gov.co>
2020-12-11 01:02:15 -0500

    grilla detalle

--- asivamosffie.web/asivamosffie/src/app/_pages/requisitos-tecnicos-construccion/components/detalle-tabla-programacion-obra/detalle-tabla-programacion-obra.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/requisitos-tecnicos-construccion/components/detalle-tabla-programacion-obra/detalle-tabla-programacion-obra.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { CommonService } from 'src/app/core/_services/common/common.service';
+import { FaseUnoConstruccionService } from 'src/app/core/_services/faseUnoConstruccion/fase-uno-construccion.service';
 import { DialogObservacionesProgramacionComponent } from '../dialog-observaciones-programacion/dialog-observaciones-programacion.component';
 
 @Component({
@@ -16,29 +18,39 @@ export class DetalleTablaProgramacionObraComponent implements OnInit {
   @ViewChild( MatPaginator, { static: true } ) paginator: MatPaginator;
   @ViewChild( MatSort, { static: true } ) sort          : MatSort;
   displayedColumns: string[] = [ 
-    'fechaCargue',
-    'totalRegistros',
-    'registrosValidos',
-    'registrosInvalidos',
+    'fechaCreacion',
+    'cantidadRegistros',
+    'cantidadRegistrosValidos',
+    'cantidadRegistrosInvalidos',
+    
     'gestion'
   ];
-  dataTable: any [] = [
-    {
-      fechaCargue: '10/08/2020',
-      totalRegistros: '5',
-      registrosValidos: '3',
-      registrosInvalidos: '2',
-      gestion: 1,
-    }
-  ]
-  constructor(private dialog: MatDialog) {
+  
+
+  @Input() contratoConstruccionId: number;
+
+  constructor(
+              private dialog: MatDialog,
+              private faseUnoConstruccionSvc: FaseUnoConstruccionService,
+              private commonService: CommonService, 
+              ) {
   }
 
   ngOnInit(): void {
-    this.dataSource                        = new MatTableDataSource(this.dataTable);
-    this.dataSource.paginator              = this.paginator;
-    this.dataSource.sort                   = this.sort;
-    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.getData();
+  };
+
+  getData () {
+    console.log( this.contratoConstruccionId )
+    if ( this.contratoConstruccionId !== 0 ) {
+      this.faseUnoConstruccionSvc.getLoadProgrammingGrid( this.contratoConstruccionId )
+      .subscribe( ( response: any[] ) => {
+        this.dataSource                        = new MatTableDataSource( response );
+        this.dataSource.paginator              = this.paginator;
+        this.dataSource.sort                   = this.sort;
+        this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+      } );
+    }
   };
 
   applyFilter ( event: Event ) {
@@ -46,14 +58,32 @@ export class DetalleTablaProgramacionObraComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   };
   
-  addObservaciones(){
+  addObservaciones( pArchivoCargueId: number, estadoCargue: string, fechaCreacion, observaciones?: string  ){
     const dialogCargarProgramacion = this.dialog.open( DialogObservacionesProgramacionComponent, {
-      width: '75em'
+      width: '75em',
+      data: { pArchivoCargueId, observaciones, estadoCargue, fechaCreacion, esFlujoInversion: false, ocultarBoton:true }
     });
+    dialogCargarProgramacion.afterClosed()
+      .subscribe( response => {
+        if ( response.realizoObservacion ) {
+          this.dataSource = new MatTableDataSource();
+          this.getData();
+        };
+      } )
+  };
 
-    dialogCargarProgramacion.afterClosed().subscribe( resp => {
-      console.log( resp );
-    } );
+  descargar( pArchivoCargueId: number ) {
+    this.commonService.getFileById( pArchivoCargueId )
+      .subscribe(respuesta => {
+        let documento = "ProgramacionObra.xlsx";
+        var text = documento,
+          blob = new Blob([respuesta], { type: 'application/octet-stream' }),
+          anchor = document.createElement('a');
+        anchor.download = documento;
+        anchor.href = window.URL.createObjectURL(blob);
+        anchor.dataset.downloadurl = ['application/octet-stream', anchor.download, anchor.href].join(':');
+        anchor.click();
+      });
   }
 
 }
