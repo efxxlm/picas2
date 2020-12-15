4c49ef0eb00f81257f04ac1a4f49e3e52b00f8c9
Julian Martinez <julian1010011010@gmail.com>
2020-12-15 17:26:45 -0500

    v37 Guardado Seguimiento Semanal☢☢

--- asivamosffie.model/Models/SeguimientoSemanalGestionObraSeguridadSalud.cs
+++ asivamosffie.model/Models/SeguimientoSemanalGestionObraSeguridadSalud.cs
@@ -25,7 +25,8 @@ namespace asivamosffie.model.Models
         public string UsuarioModificacion { get; set; }
         public DateTime? FechaModificacion { get; set; }
         public bool? Eliminado { get; set; }
-        public int CantidadAccidentes { get; set; }
+        public int? CantidadAccidentes { get; set; }
+
         public virtual SeguimientoSemanalGestionObra SeguimientoSemanalGestionObra { get; set; }
         public virtual ICollection<SeguridadSaludCausaAccidente> SeguridadSaludCausaAccidente { get; set; }
     }
