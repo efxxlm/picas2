2e1ae35ae17e323da4ec911f3fcc0ae59457b043
David Benitez <divist100@gmail.com>
2020-12-22 10:36:03 -0500

    Bugs de front

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-validar-requisitos-pago/components/dialog-observaciones-item-listchequeo/dialog-observaciones-item-listchequeo.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-validar-requisitos-pago/components/dialog-observaciones-item-listchequeo/dialog-observaciones-item-listchequeo.component.ts
@@ -31,32 +31,16 @@ export class DialogObservacionesItemListchequeoComponent implements OnInit {
     const inputChar = String.fromCharCode(event.charCode);
     return alphanumeric.test(inputChar) ? true : false;
   }
-
   maxLength(e: any, n: number) {
+    console.log(e.editor.getLength()+" "+n);
     if (e.editor.getLength() > n) {
-      e.editor.deleteText(n, e.editor.getLength());
+      e.editor.deleteText(n-1, e.editor.getLength());
     }
   }
-
-  textoLimpio(texto: string) {
-    let saltosDeLinea = 0;
-    saltosDeLinea += this.contarSaltosDeLinea(texto, '<p>');
-    saltosDeLinea += this.contarSaltosDeLinea(texto, '<li>');
-
-    if ( texto ){
-      const textolimpio = texto.replace(/<(?:.|\n)*?>/gm, '');
-      return textolimpio.length + saltosDeLinea;
+  textoLimpio(texto,n) {
+    if (texto!=undefined) {
+      return texto.getLength() > n ? n : texto.getLength();
     }
-  }
-
-  private contarSaltosDeLinea(cadena: string, subcadena: string) {
-    let contadorConcurrencias = 0;
-    let posicion = 0;
-    while ((posicion = cadena.indexOf(subcadena, posicion)) !== -1) {
-      ++contadorConcurrencias;
-      posicion += subcadena.length;
-    }
-    return contadorConcurrencias;
   }
   onSubmit() {
     console.log(this.addressForm.value);
