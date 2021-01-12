bf5fd5eb6a861fb75c5fa6538d9d9954b92e07e6
Julian Martinez <julian1010011010@gmail.com>
2021-01-12 18:32:12 -0500

    string String = "string".toString();

--- asivamosffie.services/devAsiVamosFFIEContext.cs
+++ asivamosffie.services/devAsiVamosFFIEContext.cs
@@ -165,6 +165,8 @@ namespace asivamosffie.model.Models
         public virtual DbSet<VVerificarValidarSeguimientoSemanal> VVerificarValidarSeguimientoSemanal { get; set; }
         public virtual DbSet<VigenciaAporte> VigenciaAporte { get; set; }
 
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ActuacionSeguimiento>(entity =>
