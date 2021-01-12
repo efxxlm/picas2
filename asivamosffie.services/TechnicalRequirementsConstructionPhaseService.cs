8b6b060234725044bf3d133827c7e23a2c5c138a
DiegoEstupinan <daestupinans@dane.gov.co>
2021-01-12 08:12:13 -0500

    Fix errors after merge

--- asivamosffie.services/TechnicalRequirementsConstructionPhaseService.cs
+++ asivamosffie.services/TechnicalRequirementsConstructionPhaseService.cs
@@ -1627,19 +1627,7 @@ namespace asivamosffie.services
             
         }
 
-        private Proyecto CalcularFechaInicioContrato(int pContratoConstruccionId)
-        {
-
-            Proyecto proyecto = new Proyecto();
-
-            // obtengo la informacion del proyecto
-            ContratoConstruccion contratoConstruccion = _context.ContratoConstruccion
-                                                                        .Find(pContratoConstruccionId);
-
-            return CalcularFechasContrato( contratoConstruccion.ProyectoId );
-
-            
-        }
+        
 
         #endregion private
 
