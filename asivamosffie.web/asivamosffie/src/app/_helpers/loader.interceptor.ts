23442eee38125a41832d5577b000b2dd74746e00
Carlos Molero <61335101+CharlyG360VE@users.noreply.github.com>
2021-01-13 14:54:14 -0500

    add-loadingSpinner

--- asivamosffie.web/asivamosffie/src/app/_helpers/loader.interceptor.ts
+++ asivamosffie.web/asivamosffie/src/app/_helpers/loader.interceptor.ts
@@ -7,19 +7,28 @@ import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { SpinnerLoadingComponent } from '../shared/components/spinner-loading/spinner-loading.component';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
-  constructor(
-    //private store: Store,
-    private dialog: MatDialog
-  ) {
-  }
+  
+    peticiones = 0;
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const dialogRef = this.dialog.open(SpinnerLoadingComponent);
-    return next.handle(req).pipe(
-      finalize(() => dialogRef.close())
-    );
-  }
+    constructor( private spinner: NgxSpinnerService ) {
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        this.spinner.show();
+        this.peticiones++;
+        return next.handle(req)
+            .pipe(
+                finalize( () => {
+                    this.peticiones--;
+                    if ( this.peticiones === 0 ) {
+                        this.spinner.hide();
+                    }
+                } )
+            );
+    }
+
 }
