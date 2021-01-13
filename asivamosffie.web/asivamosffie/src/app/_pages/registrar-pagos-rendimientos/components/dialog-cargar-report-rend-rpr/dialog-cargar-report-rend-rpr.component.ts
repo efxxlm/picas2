03713e4704dd3fcee35aae733c33dda2500c5c9b
David Benitez <divist100@gmail.com>
2021-01-13 11:33:20 -0500

    Fix front 4.4.3

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-pagos-rendimientos/components/dialog-cargar-report-rend-rpr/dialog-cargar-report-rend-rpr.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-pagos-rendimientos/components/dialog-cargar-report-rend-rpr/dialog-cargar-report-rend-rpr.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component';
 
 @Component({
   selector: 'app-dialog-cargar-report-rend-rpr',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-cargar-report-rend-rpr.component.scss']
 })
 export class DialogCargarReportRendRprComponent implements OnInit {
-
-  constructor() { }
+  addressForm = new FormGroup({
+    documentoFile: new FormControl()
+  });
+  boton: string = "Cargar";
+  archivo: string;
+  constructor(private router: Router, public dialog: MatDialog, private fb: FormBuilder, public matDialogRef: MatDialogRef<DialogCargarReportRendRprComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+  }
+  openDialog(modalTitle: string, modalText: string) {
+    let dialogRef = this.dialog.open(ModalDialogComponent, {
+      width: '30em',
+      data: { modalTitle, modalText }
+    });
+  }
+  openDialogNoConfirmar (modalTitle: string, modalText: string) {
+    const confirmarDialog = this.dialog.open(ModalDialogComponent, {
+      width: '40em',
+      data : { modalTitle, modalText}
+    });
+  };
+  openDialogConfirmar (modalTitle: string, modalText: string) {
+    const confirmarDialog = this.dialog.open(ModalDialogComponent, {
+      width: '40em',
+      data : { modalTitle, modalText, siNoBoton:true }
+    });
+  };
+  fileName(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.archivo = event.target.files[0].name;
+      this.addressForm.patchValue({
+        documentoFile: file
+      });
+    }
+  }
+  onSubmit() {
+    const pContrato = new FormData();
+    let pFile = this.addressForm.get('documentoFile').value;
+    pFile = pFile.name.split('.');
+    pFile = pFile[pFile.length - 1];
+    if (pFile === 'xlsx') {
+      this.openDialogConfirmar(
+        'Validación de registro',
+        ` <br>Número de registros en el archivo: <b>5</b><br>
+        Número de registros válidos: <b>5</b><br>
+        Número de registros inválidos: <b>0</b><br><br>
+        <b>¿Desea realizar el cargue de los reportes de rendimiento?</b>
+        `
+      );  
+    } else {
+      this.openDialog('', '<b>El tipo de archivo que esta intentando cargar no es permitido en la plataforma.<br>El tipo de documento soportado es .xlsx</b>');
+      return;
+    }
+  }
+  close() {
+    this.matDialogRef.close('aceptado');
   }
 
 }
--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-pagos-rendimientos/components/tabla-registrar-rendimientos-rpr/tabla-registrar-rendimientos-rpr.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-pagos-rendimientos/components/tabla-registrar-rendimientos-rpr/tabla-registrar-rendimientos-rpr.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DialogCargarReportRendRprComponent } from '../dialog-cargar-report-rend-rpr/dialog-cargar-report-rend-rpr.component';
 
 @Component({
   selector: 'app-tabla-registrar-rendimientos-rpr',
@@ -7,9 +12,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaRegistrarRendimientosRprComponent implements OnInit {
 
-  constructor() { }
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  displayedColumns: string[] = [
+    'fechaCargue',
+    'numTotalRegistros',
+    'numRegistrosValidos',
+    'numRegistrosInvalidos',
+    'registrosInconsistentes',
+    'estadoCargue',
+    'gestion'
+  ];
+  dataTable: any[] = [
+    {
+      fechaCargue: '10/08/2020',
+      numTotalRegistros: 5,
+      numRegistrosValidos: 3,
+      numRegistrosInvalidos: 2,
+      registrosInconsistentes: '---',
+      estadoCargue: 'Fallido',
+      gestion: 1
+    },
+    {
+      fechaCargue: '15/08/2020',
+      numTotalRegistros: 5,
+      numRegistrosValidos: 5,
+      numRegistrosInvalidos: 0,
+      registrosInconsistentes: 2,
+      estadoCargue: 'Valido',
+      gestion: 2
+    },
+    {
+      fechaCargue: '15/08/2020',
+      numTotalRegistros: 5,
+      numRegistrosValidos: 5,
+      numRegistrosInvalidos: 0,
+      registrosInconsistentes: 0,
+      estadoCargue: 'Valido',
+      gestion: 3
+    }
+  ];
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.dataTable);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
+      if (length === 0 || pageSize === 0) {
+        return '0 de ' + length;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      // If the start index exceeds the list length, do not try and fix the end index to the end.
+      const endIndex = startIndex < length ?
+        Math.min(startIndex + pageSize, length) :
+        startIndex + pageSize;
+      return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
+    };
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  };  
+
+  cargarNuevoReportedeRendimiento(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.height = 'auto';
+    dialogConfig.width = '865px';
+    const dialogRef = this.dialog.open(DialogCargarReportRendRprComponent, dialogConfig);
+  }
 }
