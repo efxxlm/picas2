c3d5d8375af457e0a0f0f868f9738c70fa951e85
David Benitez <divist100@gmail.com>
2021-01-20 09:58:54 -0500

    Fix front 4.3.8

--- asivamosffie.web/asivamosffie/src/app/_pages/aprobar-incorporacion-rendimientos/components/aprobar-incorporacion-rendimientos/aprobar-incorporacion-rendimientos.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/aprobar-incorporacion-rendimientos/components/aprobar-incorporacion-rendimientos/aprobar-incorporacion-rendimientos.component.ts
@@ -24,7 +24,7 @@ export class AprobarIncorporacionRendimientosComponent implements OnInit {
   dataTable: any[] = [
     {
       fechaCargue: '10/08/2020',
-      numTotalRegistrosIncorporados: '',
+      numTotalRegistrosIncorporados: 5,
       gestion: 1
     }
   ]
