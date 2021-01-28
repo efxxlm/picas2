1ce626400feccb2667c6f0b6e86ec42fc526c034
David Benitez <divist100@gmail.com>
2021-01-28 12:01:02 -0500

    Fix front-integracion 4.2.1

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-tramite-controversias-contractuales/components/ver-detalleeditar-actuacion-notai/ver-detalleeditar-actuacion-notai.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-tramite-controversias-contractuales/components/ver-detalleeditar-actuacion-notai/ver-detalleeditar-actuacion-notai.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ContractualControversyService } from 'src/app/core/_services/ContractualControversy/contractual-controversy.service';
 
 @Component({
   selector: 'app-ver-detalleeditar-actuacion-notai',
@@ -7,15 +8,47 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./ver-detalleeditar-actuacion-notai.component.scss']
 })
 export class VerDetalleeditarActuacionNotaiComponent implements OnInit {
-
+  public controversiaID = parseInt(localStorage.getItem("controversiaID"));
   idControversia: any;
   idActuacion: any;
+  tipoControversia: string;
+  fechaSolicitud: any;
+  codigoSolicitud: any;
+  numeroContrato: any;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private services: ContractualControversyService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(param => {
       this.idActuacion = param.id;
+    });
+    this.loadDataContrato(this.controversiaID);
+  }
+  loadDataContrato(id){
+    this.services.GetControversiaContractualById(id).subscribe((data:any)=>{
+      switch(data.tipoControversiaCodigo){
+        case '2':
+          this.tipoControversia = 'Terminación anticipada por imposibilidad de ejecución (TAIE) a solicitud del contratista';
+        break;
+        case '3':
+          this.tipoControversia = 'Arreglo Directo (AD) a solicitud del contratista';
+        break;
+        case '4':
+          this.tipoControversia = 'Otras controversias contractuales (OCC) a solicitud del contratista';
+        break;
+        case '5':
+          this.tipoControversia = 'Terminación anticipada por imposibilidad de ejecución (TAIE) a solicitud del contratante';
+        break;
+        case '6':
+          this.tipoControversia = 'Arreglo Directo (AD) a solicitud del contratante';
+        break;
+        case '7':
+          this.tipoControversia = 'Otras controversias contractuales (OCC) a solicitud del contratante';
+        break;
+      };
+      this.fechaSolicitud = data.fechaSolicitud;
+      this.codigoSolicitud = data.numeroSolicitud;
+      this.numeroContrato = data.contrato.numeroContrato;
     });
   }
 }
