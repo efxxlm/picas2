dc574a69e4695ae36646f3dd909e5e56dd8d7a63
Johan Steward Galeano Torres <johans.galeanot@hotmail.com>
2021-01-28 16:01:13 -0500

    tabla

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-informe-final-proyecto/components/tabla-informe-final-anexos/tabla-informe-final-anexos.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-informe-final-proyecto/components/tabla-informe-final-anexos/tabla-informe-final-anexos.component.ts
@@ -10,21 +10,6 @@ import { DialogTipoDocumentoComponent } from '../dialog-tipo-documento/dialog-ti
 import { Anexo } from 'src/app/_interfaces/proyecto-final-anexos.model';
 import { RegistrarInformeFinalProyectoService } from 'src/app/core/_services/registrar-informe-final-proyecto.service';
 
-export interface VerificacionDiaria {
-  id: string;
-  numero: string;
-  item: string;
-  estadoRequisito: string;
-}
-
-const ELEMENT_DATA: VerificacionDiaria[] = [
-  {
-    id: '1',
-    numero: '21/06/2020',
-    item: 'LJ776554',
-    estadoRequisito: '',
-  }
-];
 
 @Component({
   selector: 'app-tabla-informe-final-anexos',
@@ -32,37 +17,37 @@ const ELEMENT_DATA: VerificacionDiaria[] = [
   styleUrls: ['./tabla-informe-final-anexos.component.scss']
 })
 export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
-
+  ELEMENT_DATA : Anexo[] = [];
   @Input() id: string;
   anexos: any;
   displayedColumns: string[] = [
-    'numero',
-    'item',
-    'estadoRequisito',
-    'id'
+    'informeFinalListaChequeoId',
+    'nombre',
+    'calificacionCodigo',
+    'informeFinalInterventoriaId'
   ];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<Anexo>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  addressForm = this.fb.group({
-    informe: this.fb.array([
-      [null, Validators.required]
-    ])
-  })
+  // addressForm = this.fb.group({
+  //   informe: this.fb.array([
+  //     [null, Validators.required]
+  //   ])
+  // })
 
   estaEditando = false;
 
   estadoArray = [
-    { name: 'Cumple', value: 'cumple' },
-    { name: 'No cumple', value: 'noCumple' },
-    { name: 'No aplica', value: 'noAplica' },
+    { name: 'Cumple', value: 1 },
+    { name: 'No cumple', value: 2 },
+    { name: 'No aplica', value: 3 ,}
   ];
 
-  get informeField() {
-    return this.addressForm.get('informe') as FormArray;
-  }
+  // get informeField() {
+  //   return this.addressForm.get('informe') as FormArray;
+  // }
 
   constructor(
     private fb: FormBuilder,
@@ -77,21 +62,23 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   getInformeFinalListaChequeo (id:string) {
     this.registrarInformeFinalProyectoService.getInformeFinalListaChequeo(id)
     .subscribe(anexos => {
+      this.dataSource.data = anexos as Anexo[];
       this.anexos = anexos;
       console.log(this.anexos);
-      console.log(anexos);
     });
   }
 
+
+
   ngAfterViewInit() {
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-    // this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
-    // this.paginator._intl.nextPageLabel = 'Siguiente';
-    // this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
-    //   return (page + 1).toString() + ' de ' + length.toString();
-    // };
-    // this.paginator._intl.previousPageLabel = 'Anterior';
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.paginator._intl.nextPageLabel = 'Siguiente';
+    this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
+      return (page + 1).toString() + ' de ' + length.toString();
+    };
+    this.paginator._intl.previousPageLabel = 'Anterior';
   }
 
   applyFilter(event: Event) {
@@ -121,7 +108,7 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit() {
-    console.log(this.addressForm.value);
+    // console.log(this.addressForm.value);
     this.estaEditando = true;
     this.openDialog('', '<b>La información ha sido guardada exitosamente.</b>');
   }
