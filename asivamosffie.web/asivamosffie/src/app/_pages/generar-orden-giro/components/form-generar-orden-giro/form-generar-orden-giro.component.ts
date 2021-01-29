9a68ffe56d1093cbc691b2e9a832a6dff05fa071
CharlyG360VE <charlyg360ve@gmail.com>
2021-01-28 19:59:20 -0500

    fix-integracion-4.3.3

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-orden-giro/components/form-generar-orden-giro/form-generar-orden-giro.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-orden-giro/components/form-generar-orden-giro/form-generar-orden-giro.component.ts
@@ -1,3 +1,4 @@
+import { CommonService, Dominio } from 'src/app/core/_services/common/common.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { OrdenPagoService } from 'src/app/core/_services/ordenPago/orden-pago.service';
@@ -9,19 +10,37 @@ import { OrdenPagoService } from 'src/app/core/_services/ordenPago/orden-pago.se
 })
 export class FormGenerarOrdenGiroComponent implements OnInit {
 
+    solicitudPago: any;
+    contrato: any;
+    modalidadContratoArray: Dominio[] = [];
+
     constructor(
         private activatedRoute: ActivatedRoute,
-        private ordenPagoSvc: OrdenPagoService )
+        private ordenPagoSvc: OrdenPagoService,
+        private commonSvc: CommonService )
     {
-        this.ordenPagoSvc.getSolicitudPagoBySolicitudPagoId( this.activatedRoute.snapshot.params.id )
-            .subscribe(
-                response => {
-                    console.log( response );
-                }
-            );
+        this.commonSvc.modalidadesContrato()
+        .subscribe( response => {
+            this.modalidadContratoArray = response;
+            this.ordenPagoSvc.getSolicitudPagoBySolicitudPagoId( this.activatedRoute.snapshot.params.id )
+                .subscribe(
+                    response => {
+                        this.solicitudPago = response;
+                        this.contrato = response[ 'contratoSon' ];
+                        console.log( this.solicitudPago, this.contrato );
+                    }
+                );
+        } );
     }
 
     ngOnInit(): void {
+    }
+
+    getModalidadContrato( modalidadCodigo: string ) {
+        if ( this.modalidadContratoArray.length > 0 ) {
+            const modalidad = this.modalidadContratoArray.filter( modalidad => modalidad.codigo === modalidadCodigo );
+            return modalidad[0].nombre;
+        }
     }
 
 }
