30eee3b7a233aa1e753629ca0f8b7a4c7eccdcc5
David Benitez <divist100@gmail.com>
2021-02-03 15:41:41 -0500

    Fix front 5.3.1

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-balan-financ-trasl-recursos/components/fuentes-usos-gbftrec/fuentes-usos-gbftrec.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-balan-financ-trasl-recursos/components/fuentes-usos-gbftrec/fuentes-usos-gbftrec.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-fuentes-usos-gbftrec',
@@ -6,10 +8,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fuentes-usos-gbftrec.component.scss']
 })
 export class FuentesUsosGbftrecComponent implements OnInit {
-
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  displayedColumns: string[] = [
+    'aportante',
+    'valorAportante',
+    'fuenteAportante',
+    'uso',
+    'valorUso'
+  ];
+  tablaEjemplo: any[] = [
+    {
+      aportante: 'Alcaldía de Susacón',
+      valorAportante: '$45.000.000',
+      fuenteAportante: [
+        {
+          fuente: 'Recursos propios',
+          uso: [
+            { nombre: 'Diseño' }
+          ],
+          valorUso: [
+            { valor: '$12.000.000' }
+          ],
+        },
+        {
+          fuente: 'Contingencias',
+          uso: [
+            { nombre: 'Diseño' },
+            { nombre: 'Obra principal' }
+          ],
+          valorUso: [
+            { valor: '$12.000.000' },
+            { valor: '$21.000.000' }
+          ],
+        }
+      ],
+    },
+    {
+      aportante: 'Gobernación de Boyacá',
+      valorAportante: '$40.000.000',
+      fuenteAportante: [
+        {
+          fuente: 'Recursos propios',
+          uso: [
+            { nombre: 'Obra principal' }
+          ],
+          valorUso: [
+            { valor: '$40.000.000' },
+          ],
+        }
+      ],
+    },
+    {
+      aportante: 'FFIE',
+      valorAportante: '$20.000.000',
+      fuenteAportante: [
+        {
+          fuente: 'Contingencias',
+          uso: [
+            { nombre: 'Obra principal' }
+          ],
+          valorUso: [
+            { valor: '$20.000.000' },
+          ],
+        }
+      ],
+    }
+    /*
+    {
+      aportante: 'Gobernación de Boyacá',
+      valorAportante: '$40.000.000',
+      fuenteAportante: [
+        { fuente: 'Recursos propios' },
+      ],
+      uso: [
+        { nombre: 'Obra principal' },
+      ],
+      valorUso: [
+        { valor: '$40.000.000' },
+      ],
+    },
+    {
+      aportante: 'FFIE',
+      valorAportante: '$20.000.000',
+      fuenteAportante: [
+        { fuente: 'Contingencias' },
+      ],
+      uso: [
+        { nombre: 'Obra principal' },
+      ],
+      valorUso: [
+        { valor: '$20.000.000' },
+      ],
+    },
+    */
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    this.loadDataSource();
+  }
+  loadDataSource() {
+    this.dataSource = new MatTableDataSource(this.tablaEjemplo);
+    this.dataSource.sort = this.sort;
+    //this.dataSource.paginator = this.paginator;
+    //this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    /*
+    this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
+      if (length === 0 || pageSize === 0) {
+        return '0 de ' + length;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      // If the start index exceeds the list length, do not try and fix the end index to the end.
+      const endIndex = startIndex < length ?
+        Math.min(startIndex + pageSize, length) :
+        startIndex + pageSize;
+      return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
+    };*/
   }
 
 }
