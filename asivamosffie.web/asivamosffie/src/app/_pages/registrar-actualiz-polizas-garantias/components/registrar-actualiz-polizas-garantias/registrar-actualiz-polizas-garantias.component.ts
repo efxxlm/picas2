b1879859e28390f2d1cccb2a5a992c0788138ac8
David Benitez <divist100@gmail.com>
2021-02-10 15:57:22 -0500

    Fix front 5.4.1

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-actualiz-polizas-garantias/components/registrar-actualiz-polizas-garantias/registrar-actualiz-polizas-garantias.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-actualiz-polizas-garantias/components/registrar-actualiz-polizas-garantias/registrar-actualiz-polizas-garantias.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-registrar-actualiz-polizas-garantias',
@@ -11,36 +14,91 @@ import { Router } from '@angular/router';
 })
 export class RegistrarActualizPolizasGarantiasComponent implements OnInit {
   verAyuda = false;
+  addressForm = this.fb.group({
+    numeroContrato: [ null, Validators.compose( [ Validators.minLength(3), Validators.maxLength(10) ] ) ],
+  });
+  myFilter = new FormControl();
   dataSource = new MatTableDataSource();
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   displayedColumns: string[] = [
-    'fechaTerminacionProyecto',
-    'llaveMEN',
-    'tipoIntervencion',
-    'institucionEducativa',
-    'sede',
-    'numeroTraslados',
-    'estadoBalance',
+    'numContrato',
+    'tipoContrato',
+    'contratista',
     'gestion'
   ];
   dataTable: any[] = [
     {
-      fechaTerminacionProyecto: '09/08/2021',
-      llaveMEN: 'LL457326',
-      tipoIntervencion: 'Remodelación',
-      institucionEducativa: 'I.E Nuestra Señora Del Carmen/Única sede',
-      sede: 'Única sede',
-      numeroTraslados: '---',
-      estadoBalance: 'Sin balance validado',
+      numContrato:'N801800',
+      tipoContrato:'Interventoria',
+      contratista:'Constructora Colpatria',
       gestion: 1
     },
+    {
+      numContrato:'N801801',
+      tipoContrato:'Obra',
+      contratista:'Interventores S.A',
+      gestion: 2
+    },
+    {
+      numContrato:'N801999',
+      tipoContrato:'Obra',
+      contratista:'Constructora Colpatria',
+      gestion: 3
+    },
   ];
+  contratosList: any[] = [
+    {
+      codigo: '1',
+      numContrato: 'N801801'
+    },
+  ];
+  filteredContrato: Observable<string[]>;
+  selectedContract: any = '';
+  estaEditando = false;
   constructor(
+    private fb: FormBuilder, 
     private routes: Router
-  ) { }
+  ) { 
+    //funcion Autocomplete
+    this.filteredContrato = this.myFilter.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
 
   ngOnInit(): void {
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();    
+    if(value!="")
+    {      
+      let filtroportipo:string[]=[];
+      this.contratosList.forEach(element => {        
+        if(!filtroportipo.includes(element.numContrato))
+        {
+          filtroportipo.push(element.numContrato);
+        }
+      });
+      let ret= filtroportipo.filter(x=> x.toLowerCase().indexOf(filterValue) === 0);      
+      return ret;
+    }
+    else
+    {
+      return [];
+    }
+    
+  }
+  seleccionAutocomplete(nombre: string){
+    this.selectedContract = nombre;
+    let lista: any[] = [];
+    this.contratosList.forEach(element => {
+      if (element.numeroContrato) {
+        lista.push(element);
+      }
+    });
+    let ret = lista.filter(x => x.numeroContrato.toLowerCase() === nombre.toLowerCase());
+    console.log(ret);
     this.loadDataSource();
   }
   loadDataSource() {
@@ -48,19 +106,8 @@ export class RegistrarActualizPolizasGarantiasComponent implements OnInit {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
-    this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
-      if (length === 0 || pageSize === 0) {
-        return '0 de ' + length;
-      }
-      length = Math.max(length, 0);
-      const startIndex = page * pageSize;
-      // If the start index exceeds the list length, do not try and fix the end index to the end.
-      const endIndex = startIndex < length ?
-        Math.min(startIndex + pageSize, length) :
-        startIndex + pageSize;
-      return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
-    };
   }
+
   validarBalance(id){
     this.routes.navigate(['/gestionarBalanceFinancieroTrasladoRecursos/validarBalance', id]);
   }
