d8ec6919a91d4c999f7ac993f68108bf1e4a113e
David Benitez <divist100@gmail.com>
2021-02-18 14:27:33 -0500

    Fix integracion 4.2.1

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-tramite-controversias-contractuales/components/form-descripcion-actuacion/form-descripcion-actuacion.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-tramite-controversias-contractuales/components/form-descripcion-actuacion/form-descripcion-actuacion.component.ts
@@ -223,13 +223,6 @@ export class FormDescripcionActuacionComponent implements OnInit {
     }
     this.services.CreateEditControversiaOtros(actuacionTaiArray).subscribe((data: any) => {
       if(data.isSuccessful==true){
-        this.services.CambiarEstadoActuacionSeguimiento(data.data.controversiaActuacionId,"1").subscribe((data:any)=>{
-        });
-        /*
-        this.services.CambiarEstadoControversiaActuacion(data.data.controversiaActuacionId,"1").subscribe((a:any)=>{
-
-        });
-        */
         this.openDialog("",`<b>${ data.message }</b>`);
         this.router.navigate(['/gestionarTramiteControversiasContractuales/actualizarTramiteControversia']);
       }
