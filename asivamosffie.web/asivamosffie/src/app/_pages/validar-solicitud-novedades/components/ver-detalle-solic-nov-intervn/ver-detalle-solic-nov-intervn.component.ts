262539cf4ad7c4f860cd93dd5b25e56ecbca00f5
David Benitez <divist100@gmail.com>
2021-02-19 15:30:03 -0500

    Fix front 4.1.5
    Ajustes, faltaba pantalla ver detalle, en proceso de construccion

---
+++ asivamosffie.web/asivamosffie/src/app/_pages/validar-solicitud-novedades/components/ver-detalle-solic-nov-intervn/ver-detalle-solic-nov-intervn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-detalle-solic-nov-intervn',
+  templateUrl: './ver-detalle-solic-nov-intervn.component.html',
+  styleUrls: ['./ver-detalle-solic-nov-intervn.component.scss']
+})
+export class VerDetalleSolicNovIntervnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
