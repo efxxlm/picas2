d071a34041358fd657c368382d2c27a35a379ca6
Johan Steward Galeano Torres <johans.galeanot@hotmail.com>
2021-03-29 18:11:05 -0500

    link Ubicación (url/radicado)

--- asivamosffie.web/asivamosffie/src/app/_pages/verificar-informe-final-proyecto/components/tabla-informe-final-anexos/tabla-informe-final-anexos.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/verificar-informe-final-proyecto/components/tabla-informe-final-anexos/tabla-informe-final-anexos.component.ts
@@ -1,16 +1,21 @@
-import { Component, AfterViewInit, ViewChild, OnInit, Input } from '@angular/core'
-import { MatPaginator } from '@angular/material/paginator'
-import { MatSort } from '@angular/material/sort'
-import { MatTableDataSource } from '@angular/material/table'
-import { MatDialog } from '@angular/material/dialog'
-import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms'
-import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component'
+import { Component, AfterViewInit, ViewChild, OnInit, Input } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
+import { ModalDialogComponent } from 'src/app/shared/components/modal-dialog/modal-dialog.component';
 import { DialogObservacionesComponent } from '../dialog-observaciones/dialog-observaciones.component';
-import { VerificarInformeFinalService } from 'src/app/core/_services/verificarInformeFinal/verificar-informe-final.service'
-import { ListaChequeo } from 'src/app/_interfaces/proyecto-final-anexos.model'
-import { InformeFinal, InformeFinalAnexo, InformeFinalInterventoria, InformeFinalInterventoriaObservaciones } from 'src/app/_interfaces/informe-final';
-import { Respuesta } from 'src/app/core/_services/common/common.service'
-import { Router } from '@angular/router'
+import { VerificarInformeFinalService } from 'src/app/core/_services/verificarInformeFinal/verificar-informe-final.service';
+import { ListaChequeo } from 'src/app/_interfaces/proyecto-final-anexos.model';
+import {
+  InformeFinal,
+  InformeFinalAnexo,
+  InformeFinalInterventoria,
+  InformeFinalInterventoriaObservaciones
+} from 'src/app/_interfaces/informe-final';
+import { Respuesta } from 'src/app/core/_services/common/common.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabla-informe-final-anexos',
@@ -18,7 +23,7 @@ import { Router } from '@angular/router'
   styleUrls: ['./tabla-informe-final-anexos.component.scss']
 })
 export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
-  ELEMENT_DATA : ListaChequeo[] = [];
+  ELEMENT_DATA: ListaChequeo[] = [];
   @Input() id: number;
   @Input() llaveMen: string;
   listChequeo: any;
@@ -35,12 +40,12 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   estadoAprobacion = '0';
   registroCompletoValidacion = false;
   addressForm: FormGroup;
-  informeFinalObservacion : InformeFinalInterventoriaObservaciones[] = [];
+  informeFinalObservacion: InformeFinalInterventoriaObservaciones[] = [];
   dataSource = new MatTableDataSource<ListaChequeo>(this.ELEMENT_DATA);
   semaforo = false;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator
-  @ViewChild(MatSort) sort: MatSort
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(
     private fb: FormBuilder,
     public dialog: MatDialog,
@@ -49,22 +54,21 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   ) {}
 
   estaEditando = false;
-  noGuardado=false;
+  noGuardado = false;
 
   estadoArray = [
     { name: 'Cumple', value: 1 },
     { name: 'No cumple', value: 2 },
     { name: 'No aplica', value: 3 }
-  ]
+  ];
 
   ngOnInit(): void {
     this.getInformeFinalListaChequeoByInformeFinalId(this.id);
   }
 
-  getInformeFinalListaChequeoByInformeFinalId (id:number) {
-    this.verificarInformeFinalService.getInformeFinalListaChequeoByInformeFinalId(id)
-    .subscribe(listChequeo => {
-      if(listChequeo != null){
+  getInformeFinalListaChequeoByInformeFinalId(id: number) {
+    this.verificarInformeFinalService.getInformeFinalListaChequeoByInformeFinalId(id).subscribe(listChequeo => {
+      if (listChequeo != null) {
         this.estadoAprobacion = listChequeo[0].informeFinal.estadoAprobacion;
         this.estadoValidacion = listChequeo[0].estadoValidacion;
         this.registroCompletoValidacion = listChequeo[0].registroCompletoValidacion;
@@ -76,28 +80,31 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort
-    this.dataSource.paginator = this.paginator
-    this.paginator._intl.itemsPerPageLabel = 'Elementos por página'
-    this.paginator._intl.nextPageLabel = 'Siguiente'
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.paginator._intl.nextPageLabel = 'Siguiente';
     this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
       if (length === 0 || pageSize === 0) {
-        return '0 de ' + length
+        return '0 de ' + length;
       }
-      length = Math.max(length, 0)
-      const startIndex = page * pageSize
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
       // If the start index exceeds the list length, do not try and fix the end index to the end.
-      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize
-      return startIndex + 1 + ' - ' + endIndex + ' de ' + length
-    }
-    this.paginator._intl.previousPageLabel = 'Anterior'
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+      return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
+    };
+    this.paginator._intl.previousPageLabel = 'Anterior';
   }
 
-  openDialogObservaciones(informe:any, verDetalle: boolean) {
+  openDialogObservaciones(informe: any, verDetalle: boolean) {
     this.informeFinalObservacion = null;
     this.dataSource.data.forEach(control => {
-      if ( informe !== null && informe.informeFinalInterventoriaId === control.informeFinalInterventoriaId ) {
-        if(control.informeFinalInterventoriaObservaciones != null && control.informeFinalInterventoriaObservaciones.length > 0){
+      if (informe !== null && informe.informeFinalInterventoriaId === control.informeFinalInterventoriaId) {
+        if (
+          control.informeFinalInterventoriaObservaciones != null &&
+          control.informeFinalInterventoriaObservaciones.length > 0
+        ) {
           this.informeFinalObservacion = control.informeFinalInterventoriaObservaciones;
         }
         return;
@@ -109,18 +116,18 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
         informe: informe,
         llaveMen: this.llaveMen,
         informeFinalObservacion: this.informeFinalObservacion,
-        verDetalle : verDetalle
+        verDetalle: verDetalle
       },
-      id:'dialogObservaciones'
+      id: 'dialogObservaciones'
     });
 
     dialogRef.afterClosed().subscribe(result => {
       this.dataSource.data.forEach(control => {
-        if ( result !== null && result.id === control.informeFinalInterventoriaId ) {
+        if (result !== null && result.id === control.informeFinalInterventoriaId) {
           control.informeFinalInterventoriaObservaciones = [];
           const informeFinalInterventoriaObservaciones: InformeFinalInterventoriaObservaciones = {
-            informeFinalInterventoriaObservacionesId:result.observaciones.informeFinalInterventoriaObservacionesId,
-            informeFinalInterventoriaId:result.observaciones.informeFinalInterventoriaId,
+            informeFinalInterventoriaObservacionesId: result.observaciones.informeFinalInterventoriaObservacionesId,
+            informeFinalInterventoriaId: result.observaciones.informeFinalInterventoriaId,
             observaciones: result.observaciones.observaciones,
             esSupervision: result.observaciones.esSupervision,
             esCalificacion: result.observaciones.esCalificacion,
@@ -140,16 +147,16 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
   openDialog(modalTitle: string, modalText: string) {
     this.dialog.open(ModalDialogComponent, {
       width: '28em',
-      data: { modalTitle, modalText },
+      data: { modalTitle, modalText }
     });
   }
 
   openDialogSuccess(modalTitle: string, modalText: string) {
     const dialogRef = this.dialog.open(ModalDialogComponent, {
       width: '28em',
-      data: { modalTitle, modalText },
+      data: { modalTitle, modalText }
     });
-  
+
     dialogRef.afterClosed().subscribe(result => {
       this.router.navigate(['/verificarInformeFinalProyecto']);
     });
@@ -157,62 +164,64 @@ export class TablaInformeFinalAnexosComponent implements OnInit, AfterViewInit {
 
   onSubmit(test: boolean) {
     this.estaEditando = true;
-    this.noGuardado=false;
+    this.noGuardado = false;
     //recorre el datasource y crea modelo
     const listaInformeFinalInterventoria = [] as InformeFinal;
     listaInformeFinalInterventoria.informeFinalInterventoria = [];
     this.dataSource.data.forEach(control => {
-        const informeFinalInterventoria: InformeFinalInterventoria = {
-          informeFinalInterventoriaId: control.informeFinalInterventoriaId,
-          informeFinalId: control.informeFinalId,
-          calificacionCodigo: control.calificacionCodigo,
-          informeFinalListaChequeoId: control.informeFinalListaChequeoId,
-          informeFinalAnexo: null,
-          informeFinalInterventoriaObservaciones: control.informeFinalInterventoriaObservaciones,
-          validacionCodigo: control.validacionCodigo,
-          tieneModificacionApoyo: control.tieneModificacionApoyo
-        };
-        listaInformeFinalInterventoria.informeFinalInterventoria.push(informeFinalInterventoria);
+      const informeFinalInterventoria: InformeFinalInterventoria = {
+        informeFinalInterventoriaId: control.informeFinalInterventoriaId,
+        informeFinalId: control.informeFinalId,
+        calificacionCodigo: control.calificacionCodigo,
+        informeFinalListaChequeoId: control.informeFinalListaChequeoId,
+        informeFinalAnexo: null,
+        informeFinalInterventoriaObservaciones: control.informeFinalInterventoriaObservaciones,
+        validacionCodigo: control.validacionCodigo,
+        tieneModificacionApoyo: control.tieneModificacionApoyo
+      };
+      listaInformeFinalInterventoria.informeFinalInterventoria.push(informeFinalInterventoria);
     });
     const informeFinal: InformeFinal = {
       informeFinalId: this.dataSource.data[0].informeFinalId,
       //proyectoId: Number(this.id),
-      informeFinalInterventoria: listaInformeFinalInterventoria.informeFinalInterventoria,
+      informeFinalInterventoria: listaInformeFinalInterventoria.informeFinalInterventoria
     };
-    this.updateStateValidateInformeFinalInterventoriaByInformeFinal(informeFinal,test);
+    this.updateStateValidateInformeFinalInterventoriaByInformeFinal(informeFinal, test);
   }
 
-  updateStateValidateInformeFinalInterventoriaByInformeFinal( informeFinal: any , test: boolean) {
-    this.verificarInformeFinalService.updateStateValidateInformeFinalInterventoriaByInformeFinal(informeFinal)
-    .subscribe((respuesta: Respuesta) => {
-      if(!test){
-        this.openDialogSuccess('', respuesta.message);
-      }else{
-        this.openDialog('', respuesta.message);
-      }
-        //this.ngOnInit();
-        return;
-      },
-      err => {
-        this.openDialog('', err.message);
-        //this.ngOnInit();
-        return;
-      });
+  updateStateValidateInformeFinalInterventoriaByInformeFinal(informeFinal: any, test: boolean) {
+    this.verificarInformeFinalService
+      .updateStateValidateInformeFinalInterventoriaByInformeFinal(informeFinal)
+      .subscribe(
+        (respuesta: Respuesta) => {
+          if (!test) {
+            this.openDialogSuccess('', respuesta.message);
+          } else {
+            this.openDialog('', respuesta.message);
+          }
+          //this.ngOnInit();
+          return;
+        },
+        err => {
+          this.openDialog('', err.message);
+          //this.ngOnInit();
+          return;
+        }
+      );
   }
 
-  changeState(informe:any){
+  changeState(informe: any) {
     this.noGuardado = true;
-    if(informe){
+    if (informe) {
       //Solo cuando es devuelto con observaciones
-      if(informe.estadoValidacion === '6' && informe.aprobacionCodigo === '2'){
+      if (informe.estadoValidacion === '6' && informe.aprobacionCodigo === '2') {
         this.dataSource.data.forEach(control => {
-          if ( informe.informeFinalInterventoriaId === control.informeFinalInterventoriaId ) {
-            control.tieneModificacionApoyo =  true; 
+          if (informe.informeFinalInterventoriaId === control.informeFinalInterventoriaId) {
+            control.tieneModificacionApoyo = true;
             return;
           }
         });
       }
     }
   }
-
 }
