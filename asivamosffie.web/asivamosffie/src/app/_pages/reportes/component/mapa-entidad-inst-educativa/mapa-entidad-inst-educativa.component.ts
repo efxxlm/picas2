49d3b43c31c27e5cb252afd24c7bf714b5e7a1b9
David Benitez <divist100@gmail.com>
2021-03-29 09:52:22 -0500

    Ajustes 7.2 parametricas

--- asivamosffie.web/asivamosffie/src/app/_pages/reportes/component/mapa-entidad-inst-educativa/mapa-entidad-inst-educativa.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/reportes/component/mapa-entidad-inst-educativa/mapa-entidad-inst-educativa.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { AnyAaaaRecord } from 'dns';
+import { CommonService } from 'src/app/core/_services/common/common.service';
 
 @Component({
   selector: 'app-mapa-entidad-inst-educativa',
@@ -23,9 +26,26 @@ export class MapaEntidadInstEducativaComponent implements OnInit {
 
   ];
   gFiltro = false;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, public commonService:CommonService) { }
 
   ngOnInit(): void {
+    this.commonService.listaDepartamentos().subscribe(response=>{
+      this.departamentoArray=response;
+    });
+  }
+  getMunicipios(event: MatSelectChange) {
+    for(let i = 0; i<event.value.length; i++){
+      let arrayFinal = [];
+      this.commonService.listaMunicipiosByIdDepartamento(event.value[i].localizacionId).subscribe(respuesta => {
+        if(i > 0){
+          arrayFinal=respuesta;
+          this.municipioArray = this.municipioArray.concat(arrayFinal);
+        }
+        else{
+          this.municipioArray=respuesta;
+        }
+      });
+    }
   }
   onSubmit(){
 
