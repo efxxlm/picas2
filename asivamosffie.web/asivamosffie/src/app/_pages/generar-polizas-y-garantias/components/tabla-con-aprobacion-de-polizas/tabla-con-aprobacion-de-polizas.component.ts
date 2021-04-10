219c1eb06c93415f121b5d4e067de86840fe2e4b
CharlyG360VE <charlyg360ve@gmail.com>
2021-04-10 16:29:50 -0500

    debug polizas v4

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-polizas-y-garantias/components/tabla-con-aprobacion-de-polizas/tabla-con-aprobacion-de-polizas.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-polizas-y-garantias/components/tabla-con-aprobacion-de-polizas/tabla-con-aprobacion-de-polizas.component.ts
@@ -2,8 +2,10 @@ import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angu
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import moment from 'moment';
 import { Subscription } from 'rxjs';
 import { PolizaGarantiaService } from 'src/app/core/_services/polizaGarantia/poliza-garantia.service';
+import { EstadoPolizaCodigo } from 'src/app/_interfaces/gestionar-polizas-garantias.interface';
 
 @Component({
   selector: 'app-tabla-con-aprobacion-de-polizas',
@@ -12,60 +14,36 @@ import { PolizaGarantiaService } from 'src/app/core/_services/polizaGarantia/pol
 })
 export class TablaConAprobacionDePolizasComponent implements OnInit {
 
-  @Output() estadoSemaforo3 = new EventEmitter<string>();
-  displayedColumns: string[] = ['fechaFirma', 'numeroContrato', 'tipoSolicitud', 'estadoPoliza', 'contratoId'];
-  dataSource = new MatTableDataSource();
+    @Output() estadoSemaforo3 = new EventEmitter<string>();
+    estadoPolizaCodigo = EstadoPolizaCodigo;
+    displayedColumns: string[] = ['fechaFirma', 'numeroContrato', 'tipoSolicitud', 'estadoPoliza', 'contratoId'];
+    dataSource = new MatTableDataSource();
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    dataTable: any[] = [];
 
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+    constructor( private polizaSvc: PolizaGarantiaService) {
+        this.polizaSvc.getListGrillaContratoGarantiaPoliza( this.estadoPolizaCodigo.conAprobacion )
+            .subscribe(
+                getListGrillaContratoGarantiaPoliza => {
+                    this.estadoSemaforo3.emit( 'completo' );
 
-  dataTable: any[] = [];
-  loadDataItems: Subscription;
-  constructor(private polizaService: PolizaGarantiaService) { }
+                    getListGrillaContratoGarantiaPoliza.forEach( registro => registro.fechaFirma = registro.fechaFirma !== undefined ? ( moment( registro.fechaFirma ).format( 'DD/MM/YYYY' ) ) : '' );
 
-  ngOnInit(): void {
-    this.polizaService.GetListGrillaContratoGarantiaPoliza().subscribe((resp: any) => {
-      console.log(  )
-      let enrevisionC  = 0;
-      for (let polizas of resp) {
-        if (polizas.estadoPoliza === 'Con aprobación de pólizas') {
-          this.dataTable.push(polizas);
-          enrevisionC++;
-        };
-      };
-      if (enrevisionC === this.dataTable.length) {
-        this.estadoSemaforo3.emit('completo');
-      }
-      else{
-        this.estadoSemaforo3.emit('sin-diligenciar');
-      };
-      if(this.dataTable.length == 0){
-        this.estadoSemaforo3.emit('completo');
-      } else {
-        this.dataTable.forEach( registro => registro.fechaFirma = registro.fechaFirma.split('T')[0].split('-').reverse().join('/') );
-      }
-      this.dataSource = new MatTableDataSource(this.dataTable);
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-      this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
-      this.paginator._intl.getRangeLabel = (page, pageSize, length) => {
-        if (length === 0 || pageSize === 0) {
-          return '0 de ' + length;
-        }
-        length = Math.max(length, 0);
-        const startIndex = page * pageSize;
-        // If the start index exceeds the list length, do not try and fix the end index to the end.
-        const endIndex = startIndex < length ?
-          Math.min(startIndex + pageSize, length) :
-          startIndex + pageSize;
-        return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
-      };
-    });  
-  }
+                    this.dataSource = new MatTableDataSource( getListGrillaContratoGarantiaPoliza );
+                    this.dataSource.sort = this.sort;
+                    this.dataSource.paginator = this.paginator;
+                    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+                }
+            );
+    }
 
-  applyFilter ( event: Event ) {
-    const filterValue      = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  };
+    ngOnInit(): void {  
+    }
+
+    applyFilter ( event: Event ) {
+        const filterValue      = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
 
 }
