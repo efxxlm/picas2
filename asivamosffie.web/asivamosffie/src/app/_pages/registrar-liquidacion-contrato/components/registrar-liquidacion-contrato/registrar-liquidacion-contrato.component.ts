0ce1a427580bae9256c1fe3cf4e6607962f0e248
CharlyG360VE <charlyg360ve@gmail.com>
2021-04-15 20:01:38 -0500

    integracion 5.4.2 v2

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-liquidacion-contrato/components/registrar-liquidacion-contrato/registrar-liquidacion-contrato.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-liquidacion-contrato/components/registrar-liquidacion-contrato/registrar-liquidacion-contrato.component.ts
@@ -1,6 +1,7 @@
 import { CommonService } from 'src/app/core/_services/common/common.service';
 import { Component, OnInit } from '@angular/core';
 import { LiquidacionContratoService } from 'src/app/core/_services/liquidacionContrato/liquidacion-contrato.service';
+import { EstadoLiquidacionCodigo } from 'src/app/_interfaces/estados-liquidacion-contratos.interface';
 
 @Component({
   selector: 'app-registrar-liquidacion-contrato',
@@ -10,9 +11,13 @@ import { LiquidacionContratoService } from 'src/app/core/_services/liquidacionCo
 export class RegistrarLiquidacionContratoComponent implements OnInit {
 
     verAyuda = false;
+    estadoLiquidacionCodigo = EstadoLiquidacionCodigo;
     listaAcordeonSinRegistro: any[] = [];
     listaAcordeonEnProcesoFirma: any[] = [];
     listaAcordeonLiquidado: any[] = [];
+    semaforoSinRegistro = 'sin-diligenciar';
+    semaforoEnProceso = 'sin-diligenciar';
+    semaforoLiquidacion = 'completo';
 
     constructor(
         private commonSvc: CommonService,
@@ -24,6 +29,23 @@ export class RegistrarLiquidacionContratoComponent implements OnInit {
                 this.listaAcordeonSinRegistro = getListContractSettlemen[ 0 ];
                 this.listaAcordeonEnProcesoFirma = getListContractSettlemen[ 1 ];
                 this.listaAcordeonLiquidado = getListContractSettlemen[ 2 ];
+                // Get  semaforo sin registro de liquidacion
+                if ( this.listaAcordeonSinRegistro.length === 0 ) {
+                    this.semaforoSinRegistro = 'completo';
+                }
+                // Get semaforo en proceso de firmas
+                let enProceso = 0;
+                this.listaAcordeonEnProcesoFirma.forEach( registro => {
+                    if ( registro.estadoSolicitudCodigo === this.estadoLiquidacionCodigo.enProcesoDeFirmas || registro.estadoSolicitudCodigo === this.estadoLiquidacionCodigo.enProcesoFirmasFiduciaria ) {
+                        enProceso++;
+                    }
+                } )
+                if ( enProceso > 0 && ( enProceso < this.listaAcordeonEnProcesoFirma.length || enProceso === this.listaAcordeonEnProcesoFirma.length ) ) {
+                    this.semaforoEnProceso = 'en-proceso';
+                }
+                if ( enProceso === 0 && ( this.listaAcordeonEnProcesoFirma.length === 0 || this.listaAcordeonEnProcesoFirma.length > 0 ) ) {
+                    this.semaforoEnProceso = 'completo';
+                }
             } );
         
     }
--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-liquidacion-contrato/components/tabla-informe-final-rlc/tabla-informe-final-rlc.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-liquidacion-contrato/components/tabla-informe-final-rlc/tabla-informe-final-rlc.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { RegisterContractualLiquidationRequestService } from 'src/app/core/_services/registerContractualLiquidationRequest/register-contractual-liquidation-request.service';
+import { ListaMenuSolicitudLiquidacionId } from 'src/app/_interfaces/estados-solicitud-liquidacion-contractual';
 
 @Component({
   selector: 'app-tabla-informe-final-rlc',
@@ -9,38 +11,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./tabla-informe-final-rlc.component.scss']
 })
 export class TablaInformeFinalRlcComponent implements OnInit {
-  dataSource = new MatTableDataSource();
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  displayedColumns: string[] = [
-    'fechaEnvioInformeFinal',
-    'fechaAprobacionInformeFinal',
-    'llaveMen',
-    'tipoIntervencion',
-    'institucionEducativa',
-    'sede',
-    'gestion'
-  ];
-  dataTable: any[] = [
-    {
-      fechaEnvioInformeFinal: '29/11/2020',
-      fechaAprobacionInformeFinal: '29/11/2020',
-      llaveMen: 'LL457326',
-      tipoIntervencion: 'Remodelación',
-      institucionEducativa: 'I.E. Nuestra Señora del Carmen',
-      sede: 'Única sede',
-      id:1
-    },
-  ];
-  constructor(private router: Router) { }
 
-  ngOnInit(): void {
-    this.loadDataSource();
-  }
-  loadDataSource() {
-    this.dataSource = new MatTableDataSource(this.dataTable);
-    this.dataSource.sort = this.sort;
-  }
-  verDetalleInformeFinal(id){
-    this.router.navigate(['/registrarLiquidacionContrato/detalleInformeFinal', id]);
-  }
+    @Input() contrato: any;
+    dataSource = new MatTableDataSource();
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    displayedColumns: string[] = [
+      'fechaEnvioInformeFinal',
+      'fechaAprobacionInformeFinal',
+      'llaveMen',
+      'tipoIntervencion',
+      'institucionEducativa',
+      'sede',
+      'gestion'
+    ];
+    listaMenu = ListaMenuSolicitudLiquidacionId;
+    datosTabla = [];
+
+    constructor(
+        private router: Router,
+        private registerContractualLiquidationRequestService: RegisterContractualLiquidationRequestService )
+    { }
+
+    async ngOnInit() {
+
+        for( const contratacionProyecto of this.contrato.contratacion.contratacionProyecto ) {
+            const proyecto = await this.registerContractualLiquidationRequestService.gridInformeFinal( contratacionProyecto.contratacionProyectoId, this.listaMenu.registrarSolicitudLiquidacionContratacion ).toPromise();
+
+            if ( proyecto !== null ) {
+                proyecto.forEach( registro => {
+                    this.datosTabla.push({
+                        fechaEnvio : registro.fechaEnvio !== undefined ? registro.fechaEnvio.split('T')[0].split('-').reverse().join('/') : '',
+                        fechaAprobacion : registro.fechaAprobacion !== undefined ? registro.fechaAprobacion.split('T')[0].split('-').reverse().join('/') : '',
+                        llaveMen: registro.llaveMen,
+                        tipoIntervencion: registro.tipoIntervencion,
+                        institucionEducativa: registro.institucionEducativa,
+                        sede: registro.sede,
+                        estadoValidacion: registro.registroCompleto ? 'Con validación' : 'Sin validación',
+                        registroCompleto: registro.registroCompleto ? 'Completo' : 'Incompleto',
+                        contratacionProyectoId: contratacionProyecto.contratacionProyectoId,
+                        proyectoId: registro.proyectoId
+                    });
+                } )
+            }
+        }
+
+        this.dataSource = new MatTableDataSource( this.datosTabla );
+    }
+
 } 
