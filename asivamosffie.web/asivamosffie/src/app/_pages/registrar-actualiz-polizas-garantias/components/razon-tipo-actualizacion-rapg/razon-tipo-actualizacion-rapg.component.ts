e35310f7d48071a5b55b91af428f8d80fd964cf6
Carlos Molero <charlyg360ve@gmail.com>
2021-05-20 10:50:31 -0500

    Fix lista de garantias

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-actualiz-polizas-garantias/components/razon-tipo-actualizacion-rapg/razon-tipo-actualizacion-rapg.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-actualiz-polizas-garantias/components/razon-tipo-actualizacion-rapg/razon-tipo-actualizacion-rapg.component.ts
@@ -27,6 +27,7 @@ export class RazonTipoActualizacionRapgComponent implements OnInit {
         polizasYSeguros: [null, Validators.required],
         seguros: this.fb.array( [] )
     });
+    listaGarantias: Dominio[] = [];
     razonActualizacionArray : Dominio[] = []
     polizasYSegurosArray: Dominio[] = [];
     tipoActualizacionArray: Dominio[] = [];
@@ -58,8 +59,8 @@ export class RazonTipoActualizacionRapgComponent implements OnInit {
         this.polizasYSegurosArray.forEach( ( poliza, index ) => {
             const garantia = this.contratoPoliza.polizaGarantia.find( garantia => garantia.tipoGarantiaCodigo === poliza.codigo );
 
-            if ( garantia === undefined ) {
-                this.polizasYSegurosArray.splice( index, 1 );
+            if ( garantia !== undefined ) {
+                this.listaGarantias.push( poliza )
             }
         } )
 
