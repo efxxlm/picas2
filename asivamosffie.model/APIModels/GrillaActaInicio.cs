e42d2db5e142a42ec8f31af573c155417a092ff2
JulianMartinez <julian1010011010@gmail.com>
2021-07-16 15:36:01 -0500

    Fix

--- asivamosffie.model/APIModels/GrillaActaInicio.cs
+++ asivamosffie.model/APIModels/GrillaActaInicio.cs
@@ -9,7 +9,8 @@ namespace asivamosffie.model.APIModels
         public int ContratoId { get; set; }
         public string FechaAprobacionRequisitos { get; set; }
         public DateTime? FechaAprobacionRequisitosDate { get; set; }
-        public string NumeroContratoObra { get; set; }
+        public string NumeroContratoObra { get; set; } 
+        public string TipoContratoCodigo { get; set; }
         public string EstadoActaCodigo { get; set; }
         public string EstadoActa { get; set; }
         public string EstadoVerificacion { get; set; }
