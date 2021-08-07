e9712e07a0b76432d98540e42ce8a325c2851b45
Carlos Molero <charlyg360ve@gmail.com>
2021-08-06 22:17:22 -0500

    Integracion plantilla V3

--- asivamosffie.web/asivamosffie/src/app/_pages/generar-orden-giro/components/plantilla/plantilla.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/generar-orden-giro/components/plantilla/plantilla.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OrdenPagoService } from 'src/app/core/_services/ordenPago/orden-pago.service';
 import { TipoSolicitudCodigo } from './enums-plantilla/tipo-solicitud.enums';
-import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { FormBuilder, FormArray, Validators } from '@angular/forms';
 import humanize from 'humanize-plus';
+import { MediosPagoCodigo } from 'src/app/_interfaces/estados-solicitudPago-ordenGiro.interface';
+import { RegistrarRequisitosPagoService } from 'src/app/core/_services/registrarRequisitosPago/registrar-requisitos-pago.service';
 
 @Component({
   selector: 'app-plantilla',
@@ -14,13 +16,36 @@ import humanize from 'humanize-plus';
 export class PlantillaComponent implements OnInit {
 
     solicitudPago = undefined;
+    ordenGiroTercero = undefined;
     tipoSolicitudCodigo = TipoSolicitudCodigo;
+    listaMediosPagoCodigo = MediosPagoCodigo;
     bancosArray: Dominio[] = [];
-    formOrigen: FormGroup = this.fb.group(
+    listaMedioPago: Dominio[] = [];
+    listaBancos: Dominio[] = [];
+    terceroGiroForm = this.fb.group({
+        medioPagoGiroContrato: [null, Validators.required],
+        transferenciaElectronica: this.fb.group( {
+            ordenGiroTerceroId: [ 0 ],
+            ordenGiroTerceroTransferenciaElectronicaId: [ 0 ],
+            titularCuenta: [ '' ],
+            titularNumeroIdentificacion: [ '' ],
+            numeroCuenta: [ '' ],
+            bancoCodigo: [ null ],
+            esCuentaAhorros: [ null ]
+        } ),
+        chequeGerencia: this.fb.group( {
+            ordenGiroTerceroId: [ 0 ],
+            ordenGiroTerceroChequeGerenciaId: [ 0 ],
+            nombreBeneficiario: [ '' ],
+            numeroIdentificacionBeneficiario: [ '' ]
+        } )
+    })
+    listaDetalleGiro: { contratacionProyectoId: number, llaveMen: string, fases: any[], semaforoDetalle: string }[] = [];
+    formOrigen = this.fb.group(
         {
             aportantes: this.fb.array( [] )
         }
-    );
+    )
 
     get aportantes() {
         return this.formOrigen.get( 'aportantes' ) as FormArray;
@@ -30,7 +55,8 @@ export class PlantillaComponent implements OnInit {
         private activatedRoute: ActivatedRoute,
         private commonSvc: CommonService,
         private ordenGiroSvc: OrdenPagoService,
-        private fb: FormBuilder )
+        private fb: FormBuilder,
+        private registrarPagosSvc: RegistrarRequisitosPagoService )
     {
         this.getOrdenGiro()
     }
@@ -41,10 +67,96 @@ export class PlantillaComponent implements OnInit {
     async getOrdenGiro() {
         const solicitudPago = await this.ordenGiroSvc.getSolicitudPagoBySolicitudPagoId( this.activatedRoute.snapshot.params.id ).toPromise()
         this.bancosArray = await this.commonSvc.listaBancos().toPromise()
+        this.solicitudPago = solicitudPago
+
+        this.getProyectos()
         this.getOrigen()
+        this.getDataTerceroGiro()
 
         console.log( solicitudPago )
-        this.solicitudPago = solicitudPago
+    }
+
+    async getProyectos() {
+        // Peticion asincrona de los proyectos por contratoId
+        const getProyectosByIdContrato: any[] = await this.registrarPagosSvc.getProyectosByIdContrato( this.solicitudPago.contratoId ).toPromise();
+        const LISTA_PROYECTOS: any[] = getProyectosByIdContrato[1];
+        const solicitudPagoFase: any[] = this.solicitudPago.solicitudPagoRegistrarSolicitudPago[ 0 ].solicitudPagoFase;
+
+        LISTA_PROYECTOS.forEach( proyecto => {
+            // Objeto Proyecto que se agregara al array listaDetalleGiro
+            const PROYECTO = {
+                semaforoDetalle: 'sin-diligenciar',
+                contratacionProyectoId: proyecto.contratacionProyectoId,
+                llaveMen: proyecto.llaveMen,
+                fases: []
+            }
+
+            const listFase = solicitudPagoFase.filter( fase => fase.contratacionProyectoId === proyecto.contratacionProyectoId )
+            PROYECTO.fases = listFase
+
+            if ( PROYECTO.fases.length > 0 ) {
+                this.listaDetalleGiro.push( PROYECTO )
+            }
+        } )
+    }
+
+    getDataTerceroGiro() {
+        this.commonSvc.listaMediosPago()
+        .subscribe( listaMediosPago => {
+            this.listaMedioPago = listaMediosPago;
+
+            this.commonSvc.listaBancos()
+                .subscribe( async listaBancos => {
+                    this.listaBancos = listaBancos;
+
+                    if ( this.solicitudPago.ordenGiro !== undefined ) {
+                        if ( this.solicitudPago.ordenGiro.ordenGiroTercero !== undefined ) {
+                            if ( this.solicitudPago.ordenGiro.ordenGiroTercero.length > 0 ) {
+                                this.ordenGiroTercero = this.solicitudPago.ordenGiro.ordenGiroTercero[0];
+                                // Get data tercero de giro
+                                const medioPago = this.listaMedioPago.find( medio => medio.codigo === this.ordenGiroTercero.medioPagoGiroCodigo );
+
+                                if ( medioPago !== undefined ) {
+                                    this.terceroGiroForm.get( 'medioPagoGiroContrato' ).setValue( medioPago.codigo );
+                                }
+
+                                if ( this.ordenGiroTercero.ordenGiroTerceroTransferenciaElectronica !== undefined ) {
+                                    if ( this.ordenGiroTercero.ordenGiroTerceroTransferenciaElectronica.length > 0 ) {
+                                        const ordenGiroTerceroTransferenciaElectronica = this.ordenGiroTercero.ordenGiroTerceroTransferenciaElectronica[0];
+
+                                        this.terceroGiroForm.get( 'transferenciaElectronica' ).setValue(
+                                            {
+                                                ordenGiroTerceroId: 0,
+                                                ordenGiroTerceroTransferenciaElectronicaId: ordenGiroTerceroTransferenciaElectronica.ordenGiroTerceroTransferenciaElectronicaId,
+                                                titularCuenta: ordenGiroTerceroTransferenciaElectronica.titularCuenta !== undefined ? ordenGiroTerceroTransferenciaElectronica.titularCuenta : '',
+                                                titularNumeroIdentificacion: ordenGiroTerceroTransferenciaElectronica.titularNumeroIdentificacion !== undefined ? ordenGiroTerceroTransferenciaElectronica.titularNumeroIdentificacion : '',
+                                                numeroCuenta: ordenGiroTerceroTransferenciaElectronica.numeroCuenta !== undefined ? ordenGiroTerceroTransferenciaElectronica.numeroCuenta : '',
+                                                bancoCodigo: ordenGiroTerceroTransferenciaElectronica.bancoCodigo !== undefined ? ordenGiroTerceroTransferenciaElectronica.bancoCodigo : null,
+                                                esCuentaAhorros: ordenGiroTerceroTransferenciaElectronica.esCuentaAhorros !== undefined ? ordenGiroTerceroTransferenciaElectronica.esCuentaAhorros : null
+                                            }
+                                        )
+                                    }
+                                }
+
+                                if ( this.ordenGiroTercero.ordenGiroTerceroChequeGerencia !== undefined ) {
+                                    if ( this.ordenGiroTercero.ordenGiroTerceroChequeGerencia.length > 0 ) {
+                                        const ordenGiroTerceroChequeGerencia = this.ordenGiroTercero.ordenGiroTerceroChequeGerencia[0];
+                                        
+                                        this.terceroGiroForm.get( 'chequeGerencia' ).setValue(
+                                            {
+                                                ordenGiroTerceroId: 0,
+                                                ordenGiroTerceroChequeGerenciaId: ordenGiroTerceroChequeGerencia.ordenGiroTerceroChequeGerenciaId,
+                                                nombreBeneficiario: ordenGiroTerceroChequeGerencia.nombreBeneficiario !== undefined ? ordenGiroTerceroChequeGerencia.nombreBeneficiario : '',
+                                                numeroIdentificacionBeneficiario: ordenGiroTerceroChequeGerencia.numeroIdentificacionBeneficiario !== undefined ? ordenGiroTerceroChequeGerencia.numeroIdentificacionBeneficiario : ''
+                                            }
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } );
+        } );
     }
 
     async getOrigen() {
@@ -75,6 +187,7 @@ export class PlantillaComponent implements OnInit {
                             } );
 
                             for ( const aportante of listaAportantes ) {
+
                                 const nombreAportante = dataAportantes.listaNombreAportante.find( nombreAportante => nombreAportante.cofinanciacionAportanteId === aportante.aportanteId );
                                 const tipoAportante = dataAportantes.listaTipoAportante.find( tipoAportante => tipoAportante.dominioId === nombreAportante.tipoAportanteId );
                                 const fuente = await this.ordenGiroSvc.getFuentesDeRecursosPorAportanteId( nombreAportante.cofinanciacionAportanteId ).toPromise();
