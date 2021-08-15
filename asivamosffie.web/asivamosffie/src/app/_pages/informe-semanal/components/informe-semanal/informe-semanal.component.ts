9907627685a6e78e642453d8fb7a841d9b7309a4
Johan Steward Galeano Torres <johans.galeanot@hotmail.com>
2021-08-15 14:57:15 -0500

    ajuste plantilla semanal

--- asivamosffie.web/asivamosffie/src/app/_pages/informe-semanal/components/informe-semanal/informe-semanal.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/informe-semanal/components/informe-semanal/informe-semanal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+// import jsPDF from 'jspdf';
+// import html2canvas from 'html2canvas';
+import { CommonService } from 'src/app/core/_services/common/common.service';
 
 @Component({
   selector: 'app-informe-semanal',
@@ -8,7 +9,9 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./informe-semanal.component.scss']
 })
 export class InformeSemanalComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private commonSvc: CommonService,
+  ) {}
 
   ngOnInit(): void {}
 
@@ -22,5 +25,32 @@ export class InformeSemanalComponent implements OnInit {
     //   pdf.save('Filename.pdf');
     // });
     console.log(document.getElementById('example-pdf').innerHTML);
+    const pdfHTML = document.getElementById('example-pdf').innerHTML;
+
+    const pdf = {
+      EsHorizontal: true,
+      MargenArriba: 2,
+      MargenAbajo: 2,
+      MargenDerecha: 2,
+      MargenIzquierda: 2,
+      Contenido: pdfHTML
+    };
+
+    this.commonSvc.GetHtmlToPdf(pdf).subscribe(
+      response => {
+        const documento = `OrdernGiro.pdf`;
+        const text = documento,
+          blob = new Blob([response], { type: 'application/pdf' }),
+          anchor = document.createElement('a');
+        anchor.download = documento;
+        anchor.href = window.URL.createObjectURL(blob);
+        anchor.dataset.downloadurl = ['application/pdf', anchor.download, anchor.href].join(':');
+        anchor.click();
+      },
+      e => {
+        console.log(e);
+      }
+    );
   }
+
 }
