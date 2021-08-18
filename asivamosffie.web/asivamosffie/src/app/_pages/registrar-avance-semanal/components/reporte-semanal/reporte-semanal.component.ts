07592aee51d2f11d8b013256fb3e9f790ee59829
divixhd <30916390+divixhd@users.noreply.github.com>
2021-08-17 19:08:02 -0500

    Ajustes template

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-avance-semanal/components/reporte-semanal/reporte-semanal.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-avance-semanal/components/reporte-semanal/reporte-semanal.component.ts
@@ -12,6 +12,16 @@ export class ReporteSemanalComponent implements OnInit {
 
   contratacionProyectoId: string;
   seguimientoSemanalId: string;
+  dataReporteSemanal: any;
+  seguimientoSemanalGestionObraId: number;
+  cantidadActividades = 0;
+  gestionObraAmbiental: any;
+  gestionAmbiental: boolean;
+  gestionAmbientalId = 0; // ID gestion ambiental.
+  manejoMaterialInsumoId = 0; // ID manejo de materiales  e insumos.
+  residuosConstruccionId = 0; // ID residuos de construccion.
+  residuosPeligrososId = 0; // ID residuos peligrosos.
+  manejoOtrosId = 0; // ID manejo de otros.
 
   constructor(
     private router: Router,
@@ -22,8 +32,8 @@ export class ReporteSemanalComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      this.contratacionProyectoId = params.contratacionProyectoId;
-      this.seguimientoSemanalId = params.seguimientoSemanalId;
+      this.contratacionProyectoId = params.pContratacionProyectoId;
+      this.seguimientoSemanalId = params.pSeguimientoSemanalId;
       console.log(this.contratacionProyectoId);
       console.log(this.seguimientoSemanalId);
       this.getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(this.contratacionProyectoId, this.seguimientoSemanalId)
@@ -32,9 +42,86 @@ export class ReporteSemanalComponent implements OnInit {
   }
 
   getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId( pContratacionProyectoId, pSeguimientoSemanalId ) {
+    //0-156
+    console.log(pContratacionProyectoId);
     this.registrarAvanceSemanalService.getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(pContratacionProyectoId, pSeguimientoSemanalId).subscribe(
       response => {
-        console.log(response)
+        this.dataReporteSemanal = response;
+        if ( this.dataReporteSemanal !== undefined ) {
+          this.seguimientoSemanalId = this.dataReporteSemanal.seguimientoSemanalId;
+          this.seguimientoSemanalGestionObraId =  this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0 ?
+          this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraId : 0;
+          if ( this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0 && this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental.length > 0 ) {
+              this.cantidadActividades = 0;
+              this.gestionObraAmbiental = this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental[0];
+
+              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental !== undefined ) {
+                  this.gestionAmbiental = true;
+              } else {
+                  this.gestionAmbiental = false;
+              }
+
+              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental === false ) {
+                  // ID gestionAmbiental
+                  this.gestionAmbientalId = this.gestionObraAmbiental.seguimientoSemanalGestionObraAmbientalId;
+                  /* Verificacion con Julian Martinez y John Portela
+                  if ( this.esVerDetalle === false ) {
+                      this.registrarAvanceSemanalService.getObservacionSeguimientoSemanal(parseInt(this.seguimientoSemanalId), this.gestionAmbientalId, this.tipoObservacionAmbiental.gestionAmbientalCodigo )
+                          .subscribe(
+                              response => {
+                                  this.obsApoyo = response.find( obs => obs.archivada === false && obs.esSupervisor === false );
+                                  this.obsSupervisor = response.find( obs => obs.archivada === false && obs.esSupervisor === true );
+                                  this.historialGestionAmbiental = response;
+
+                                  if ( this.obsApoyo !== undefined || this.obsSupervisor !== undefined ) {
+                                      this.tieneObservacion.emit();
+                                  }
+
+                                  this.tablaHistorialgestionAmbiental = new MatTableDataSource( this.historialGestionAmbiental );
+                              }
+                          );
+                  }
+                  */
+              }
+              if ( this.gestionObraAmbiental.tieneManejoMaterialesInsumo === true ) {
+                  // ID manejo de materiales e insumos
+                  if ( this.gestionObraAmbiental.manejoMaterialesInsumo !== undefined ) {
+                      this.manejoMaterialInsumoId = this.gestionObraAmbiental.manejoMaterialesInsumo.manejoMaterialesInsumosId;
+                  }
+
+                  this.cantidadActividades++;
+              }
+              if ( this.gestionObraAmbiental.tieneManejoResiduosConstruccionDemolicion === true ) {
+                  // ID residuos construccion
+                  if ( this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion !== undefined ) {
+                      this.residuosConstruccionId = this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion.manejoResiduosConstruccionDemolicionId;
+                  }
+
+                  this.cantidadActividades++;
+              }
+              if ( this.gestionObraAmbiental.tieneManejoResiduosPeligrososEspeciales === true ) {
+                  // ID residuos peligrosos
+                  if ( this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales !== undefined ) {
+                      this.residuosPeligrososId = this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales.manejoResiduosPeligrososEspecialesId;
+                  }
+
+                  this.cantidadActividades++;
+              }
+              if ( this.gestionObraAmbiental.tieneManejoOtro === true ) {
+                  // ID manejo de otros
+                  if ( this.gestionObraAmbiental.manejoOtro !== undefined ) {
+                      this.manejoOtrosId = this.gestionObraAmbiental.manejoOtro.manejoOtroId;
+                  }
+
+                  this.cantidadActividades++;
+              }
+              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental === true ) {
+                  if ( this.cantidadActividades > 0 ) {
+                    this.cantidadActividades = this.cantidadActividades;
+                  }
+              }
+          }
+      }
     })
   }
 
