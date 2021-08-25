18c8b908a9121e170b6743ecfa7b712271c08ba5
laura <lalcoto@gmail.com>
2021-08-25 13:39:12 -0500

    context

---
+++ asivamosffie.model/Models/VAportanteFuente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asivamosffie.model.Models
+{
+    public partial class VAportanteFuente
+    {
+        public int ContratoId { get; set; }
+        public int? CofinanciacionAportanteId { get; set; }
+        public decimal? Valor { get; set; }
+        public int? FuenteFinanciacionId { get; set; }
+        public string FuenteRecursosCodigo { get; set; }
+        public string FuenteRecursos { get; set; }
+    }
+}
---
+++ asivamosffie.model/Models/VPagosSolicitudXodgXcontratacionXproyectoXuso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asivamosffie.model.Models
+{
+    public partial class VPagosSolicitudXodgXcontratacionXproyectoXuso
+    {
+        public bool? Pagado { get; set; }
+        public int ContratacionId { get; set; }
+        public int? ContratacionProyectoId { get; set; }
+        public int ProyectoId { get; set; }
+        public string NumeroDrp { get; set; }
+        public string TipoUsoCodigo { get; set; }
+        public string Uso { get; set; }
+        public bool EsPreconstruccion { get; set; }
+        public decimal? SaldoUso { get; set; }
+    }
+}
