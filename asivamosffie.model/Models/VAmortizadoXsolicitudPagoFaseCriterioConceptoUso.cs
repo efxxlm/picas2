94059403fd9e22fc3a045598c2a1a5ecb71e52c2
laura <lalcoto@gmail.com>
2021-10-04 11:51:38 -0500

    context

---
+++ asivamosffie.model/Models/VAmortizadoXsolicitudPagoFaseCriterioConceptoUso.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asivamosffie.model.Models
+{
+    public partial class VAmortizadoXsolicitudPagoFaseCriterioConceptoUso
+    {
+        public int? SolicitudPagoFaseCriterioConceptoPagoId { get; set; }
+        public decimal? ValorAmortizacion { get; set; }
+    }
+}
