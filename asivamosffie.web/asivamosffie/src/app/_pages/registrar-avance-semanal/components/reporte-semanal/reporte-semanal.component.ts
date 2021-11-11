0223b150cb73573db839fbd7a96d143ef83a3e71
Johan Steward Galeano Torres <johans.galeanot@hotmail.com>
2021-11-11 13:06:25 -0500

    reporte semanal

--- asivamosffie.web/asivamosffie/src/app/_pages/registrar-avance-semanal/components/reporte-semanal/reporte-semanal.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/registrar-avance-semanal/components/reporte-semanal/reporte-semanal.component.ts
@@ -9,10 +9,9 @@ import { RegistrarAvanceSemanalService } from 'src/app/core/_services/registrarA
   selector: 'app-reporte-semanal',
   templateUrl: './reporte-semanal.component.html',
   styleUrls: ['./reporte-semanal.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 })
 export class ReporteSemanalComponent implements OnInit {
-
   contratacionProyectoId: string;
   seguimientoSemanalId: string;
   dataReporteSemanal: any;
@@ -40,46 +39,63 @@ export class ReporteSemanalComponent implements OnInit {
     private route: ActivatedRoute,
     private commonSvc: CommonService,
     private registrarAvanceSemanalService: RegistrarAvanceSemanalService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.contratacionProyectoId = params.pContratacionProyectoId;
       this.seguimientoSemanalId = params.pSeguimientoSemanalId;
-      console.log(this.contratacionProyectoId);
-      console.log(this.seguimientoSemanalId);
-      this.getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(this.contratacionProyectoId, this.seguimientoSemanalId)
+      // console.log(this.contratacionProyectoId);
+      // console.log(this.seguimientoSemanalId);
+      this.getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(
+        this.contratacionProyectoId,
+        this.seguimientoSemanalId
+      );
     });
   }
 
-  getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId( pContratacionProyectoId, pSeguimientoSemanalId ) {
+  getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(
+    pContratacionProyectoId,
+    pSeguimientoSemanalId
+  ) {
     //0-156
-    console.log(pContratacionProyectoId);
-    this.registrarAvanceSemanalService.getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(pContratacionProyectoId, pSeguimientoSemanalId).subscribe(
-      response => {
+    // console.log(pContratacionProyectoId);
+    this.registrarAvanceSemanalService
+      .getLastSeguimientoSemanalContratacionProyectoIdOrSeguimientoSemanalId(
+        pContratacionProyectoId,
+        pSeguimientoSemanalId
+      )
+      .subscribe(response => {
         this.dataReporteSemanal = response;
-        if ( this.dataReporteSemanal !== undefined ) {
-          this.infoGeneralObra = this.dataReporteSemanal.informacionGeneral[0];
-          this.infoGeneralInterventoria = this.dataReporteSemanal.informacionGeneral[1];
+        console.log(this.dataReporteSemanal);
+        if (this.dataReporteSemanal !== undefined) {
+          this.infoGeneralObra = this.dataReporteSemanal.informacionGeneral[0][0];
+          this.infoGeneralInterventoria = this.dataReporteSemanal.informacionGeneral[1][0];
           this.seguimientoSemanalId = this.dataReporteSemanal.seguimientoSemanalId;
           this.avanceFisico = this.dataReporteSemanal.avanceFisico;
           this.calcularTotal(this.avanceFisico);
-          this.seguimientoSemanalGestionObraId =  this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0 ?
-          this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraId : 0;
-          if ( this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0 && this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental.length > 0 ) {
-              this.cantidadActividades = 0;
-              this.gestionObraAmbiental = this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental[0];
+          this.seguimientoSemanalGestionObraId =
+            this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0
+              ? this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraId
+              : 0;
+          if (
+            this.dataReporteSemanal.seguimientoSemanalGestionObra.length > 0 &&
+            this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental.length > 0
+          ) {
+            this.cantidadActividades = 0;
+            this.gestionObraAmbiental =
+              this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraAmbiental[0];
 
-              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental !== undefined ) {
-                  this.gestionAmbiental = true;
-              } else {
-                  this.gestionAmbiental = false;
-              }
+            if (this.gestionObraAmbiental.seEjecutoGestionAmbiental !== undefined) {
+              this.gestionAmbiental = true;
+            } else {
+              this.gestionAmbiental = false;
+            }
 
-              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental === false ) {
-                  // ID gestionAmbiental
-                  this.gestionAmbientalId = this.gestionObraAmbiental.seguimientoSemanalGestionObraAmbientalId;
-                  /* Verificacion con Julian Martinez y John Portela
+            if (this.gestionObraAmbiental.seEjecutoGestionAmbiental === false) {
+              // ID gestionAmbiental
+              this.gestionAmbientalId = this.gestionObraAmbiental.seguimientoSemanalGestionObraAmbientalId;
+              /* Verificacion con Julian Martinez y John Portela
                   if ( this.esVerDetalle === false ) {
                       this.registrarAvanceSemanalService.getObservacionSeguimientoSemanal(parseInt(this.seguimientoSemanalId), this.gestionAmbientalId, this.tipoObservacionAmbiental.gestionAmbientalCodigo )
                           .subscribe(
@@ -97,54 +113,60 @@ export class ReporteSemanalComponent implements OnInit {
                           );
                   }
                   */
+            }
+            if (this.gestionObraAmbiental.tieneManejoMaterialesInsumo === true) {
+              // ID manejo de materiales e insumos
+              if (this.gestionObraAmbiental.manejoMaterialesInsumo !== undefined) {
+                this.manejoMaterialInsumoId =
+                  this.gestionObraAmbiental.manejoMaterialesInsumo.manejoMaterialesInsumosId;
               }
-              if ( this.gestionObraAmbiental.tieneManejoMaterialesInsumo === true ) {
-                  // ID manejo de materiales e insumos
-                  if ( this.gestionObraAmbiental.manejoMaterialesInsumo !== undefined ) {
-                      this.manejoMaterialInsumoId = this.gestionObraAmbiental.manejoMaterialesInsumo.manejoMaterialesInsumosId;
-                  }
 
-                  this.cantidadActividades++;
+              this.cantidadActividades++;
+            }
+            if (this.gestionObraAmbiental.tieneManejoResiduosConstruccionDemolicion === true) {
+              // ID residuos construccion
+              if (this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion !== undefined) {
+                this.residuosConstruccionId =
+                  this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion.manejoResiduosConstruccionDemolicionId;
               }
-              if ( this.gestionObraAmbiental.tieneManejoResiduosConstruccionDemolicion === true ) {
-                  // ID residuos construccion
-                  if ( this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion !== undefined ) {
-                      this.residuosConstruccionId = this.gestionObraAmbiental.manejoResiduosConstruccionDemolicion.manejoResiduosConstruccionDemolicionId;
-                  }
 
-                  this.cantidadActividades++;
+              this.cantidadActividades++;
+            }
+            if (this.gestionObraAmbiental.tieneManejoResiduosPeligrososEspeciales === true) {
+              // ID residuos peligrosos
+              if (this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales !== undefined) {
+                this.residuosPeligrososId =
+                  this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales.manejoResiduosPeligrososEspecialesId;
               }
-              if ( this.gestionObraAmbiental.tieneManejoResiduosPeligrososEspeciales === true ) {
-                  // ID residuos peligrosos
-                  if ( this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales !== undefined ) {
-                      this.residuosPeligrososId = this.gestionObraAmbiental.manejoResiduosPeligrososEspeciales.manejoResiduosPeligrososEspecialesId;
-                  }
 
-                  this.cantidadActividades++;
+              this.cantidadActividades++;
+            }
+            if (this.gestionObraAmbiental.tieneManejoOtro === true) {
+              // ID manejo de otros
+              if (this.gestionObraAmbiental.manejoOtro !== undefined) {
+                this.manejoOtrosId = this.gestionObraAmbiental.manejoOtro.manejoOtroId;
               }
-              if ( this.gestionObraAmbiental.tieneManejoOtro === true ) {
-                  // ID manejo de otros
-                  if ( this.gestionObraAmbiental.manejoOtro !== undefined ) {
-                      this.manejoOtrosId = this.gestionObraAmbiental.manejoOtro.manejoOtroId;
-                  }
 
-                  this.cantidadActividades++;
+              this.cantidadActividades++;
+            }
+            if (this.gestionObraAmbiental.seEjecutoGestionAmbiental === true) {
+              if (this.cantidadActividades > 0) {
+                this.cantidadActividades = this.cantidadActividades;
               }
-              if ( this.gestionObraAmbiental.seEjecutoGestionAmbiental === true ) {
-                  if ( this.cantidadActividades > 0 ) {
-                    this.cantidadActividades = this.cantidadActividades;
-                  }
-              }
+            }
           }
-          this.gestionCalidad = this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraCalidad[0];
-          this.gestionSST = this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraSeguridadSalud[0];
-          this.gestionSocial = this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraSocial[0];
+          this.gestionCalidad =
+            this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraCalidad[0];
+          this.gestionSST =
+            this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraSeguridadSalud[0];
+          this.gestionSocial =
+            this.dataReporteSemanal.seguimientoSemanalGestionObra[0].seguimientoSemanalGestionObraSocial[0];
           this.actividadesARealizar = this.dataReporteSemanal.seguimientoSemanalReporteActividad[0];
           this.registroFotografico = this.dataReporteSemanal.seguimientoSemanalRegistroFotografico[0];
-      }
-    })
+        }
+      });
   }
-  calcularTotal(data){
+  calcularTotal(data) {
     let total = 0;
     data.forEach(element => {
       console.log(element);
@@ -154,6 +176,7 @@ export class ReporteSemanalComponent implements OnInit {
     console.log(this.sumaTotal);
   }
   downloadPDF() {
+    window.print();
     /*
    html2canvas( document.getElementById( 'example-pdf' ) ).then(canvas => {
        const contentDataURL = canvas.toDataURL('image/jpeg', 1.0)
@@ -164,46 +187,44 @@ export class ReporteSemanalComponent implements OnInit {
       pdf.save('Filename.pdf');
     });
     */
-    console.log(document.getElementById('example-pdf').innerHTML);
-    const pdfHTML = document.getElementById('example-pdf').innerHTML;
-    const pdf = {
-      EsHorizontal: true,
-      MargenArriba: 2,
-      MargenAbajo: 2,
-      MargenDerecha: 2,
-      MargenIzquierda: 2,
-      Contenido: pdfHTML
-    };
+    // console.log(document.getElementById('example-pdf').innerHTML);
+    // const pdfHTML = document.getElementById('example-pdf').innerHTML;
+    // const pdf = {
+    //   EsHorizontal: true,
+    //   MargenArriba: 2,
+    //   MargenAbajo: 2,
+    //   MargenDerecha: 2,
+    //   MargenIzquierda: 2,
+    //   Contenido: pdfHTML
+    // };
     /*
     public int PlantillaId { get; set; }
-public string Nombre { get; set; }
-public string Codigo { get; set; }
-public string Contenido { get; set; }
-public double? MargenArriba { get; set; }
-public double? MargenAbajo { get; set; }
-public double? MargenDerecha { get; set; }
-public double? MargenIzquierda { get; set; }
-public int? EncabezadoId { get; set; }
-public int? PieDePaginaId { get; set; }
-public int? TipoPlantillaId { get; set; }
-*/
+    public string Nombre { get; set; }
+    public string Codigo { get; set; }
+    public string Contenido { get; set; }
+    public double? MargenArriba { get; set; }
+    public double? MargenAbajo { get; set; }
+    public double? MargenDerecha { get; set; }
+    public double? MargenIzquierda { get; set; }
+    public int? EncabezadoId { get; set; }
+    public int? PieDePaginaId { get; set; }
+    public int? TipoPlantillaId { get; set; }
+    */
 
-    this.commonSvc.GetHtmlToPdf(pdf).subscribe(
-      response => {
-        const documento = `Seguimiento Semanal.pdf`;
-        const text = documento,
-          blob = new Blob([response], { type: 'application/pdf' }),
-          anchor = document.createElement('a');
-        anchor.download = documento;
-        anchor.href = window.URL.createObjectURL(blob);
-        anchor.dataset.downloadurl = ['application/pdf', anchor.download, anchor.href].join(':');
-        anchor.click();
-      },
-      e => {
-        console.log(e);
-      }
-    );
-    
+    // this.commonSvc.GetHtmlToPdf(pdf).subscribe(
+    //   response => {
+    //     const documento = `Seguimiento Semanal.pdf`;
+    //     const text = documento,
+    //       blob = new Blob([response], { type: 'application/pdf' }),
+    //       anchor = document.createElement('a');
+    //     anchor.download = documento;
+    //     anchor.href = window.URL.createObjectURL(blob);
+    //     anchor.dataset.downloadurl = ['application/pdf', anchor.download, anchor.href].join(':');
+    //     anchor.click();
+    //   },
+    //   e => {
+    //     console.log(e);
+    //   }
+    // );
   }
-
 }
