8436e2608ff08fcccb2ca830171757b3970f766e
JulianMartinez <julian1010011010@gmail.com>
2021-12-17 15:37:41 -0500

    Fix

--- asivamosffie.model/AditionalModels/SeguimientoSemanalFinancieroXMes.cs
+++ asivamosffie.model/AditionalModels/SeguimientoSemanalFinancieroXMes.cs
@@ -9,9 +9,10 @@ namespace asivamosffie.model.AditionalModels
 		public int? SeguimientoSemanalId {get; set;}
 		public int? ContratacionProyectoId { get; set;}
 		public decimal? Valor { get; set; }
-		public int? NumeroMes { get; set; }
-		public DateTime? FechaInicio { get; set; }
-		public DateTime? FechaFin { get; set; }
+		public int? NumeroSemana { get; set; } 
 		public decimal? ValorEjecutado { get; set;}
 	}
 }
+
+
+ 
