17c4aa48f165839ae8975ae6afc15d8b62f1c7e7
JMC <julian1010011010@gmail.com>
2022-01-17 17:50:07 -0500

    Fix version

--- asivamosffie.web/asivamosffie/src/app/app.component.ts
+++ asivamosffie.web/asivamosffie/src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
+}) 
 export class AppComponent {
   title = 'asivamosffie';
+ 
+  ngOnInit(): void {
+    console.log("Versión Front: 1  Back: 1"); 
+  }
 }
