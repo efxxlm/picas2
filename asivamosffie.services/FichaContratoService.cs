057810ac6a0020299a00f9c9d4b53ced7c2780df
JMC <julian1010011010@gmail.com>
2022-01-21 18:09:35 -0500

    Fix

---
+++ asivamosffie.services/FichaContratoService.cs
@@ -0,0 +1,18 @@
+﻿using asivamosffie.model.APIModels;
+using asivamosffie.model.Models; 
+using asivamosffie.services.Helpers.Constant;
+using asivamosffie.services.Helpers.Enumerator;
+using asivamosffie.services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Z.EntityFramework.Plus; 
+namespace asivamosffie.services
+{
+    public class FichaContratoService : IFichaContratoService
+    {
+      
+    }
+}
