1ee62eb27bfc79e4c69e395d461039c1def1ed39
laura <lalcoto@gmail.com>
2022-01-28 14:18:10 -0500

    Fecha versión

--- asivamosffie.web/asivamosffie/src/app/app.component.ts
+++ asivamosffie.web/asivamosffie/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonService } from './core/_services/common/common.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,9 @@ export class AppComponent {
     ){}
 
   ngOnInit(): void {
-    this.commonSvc.getVersion().subscribe( result => { console.log("Versión Front: "+ result.front + " Back: " + result.back);});
+    this.commonSvc.getVersion().subscribe( result => {
+       console.log("Versión Front: "+ result.front + " Back: " + result.back);
+       console.log("Fecha despliegue: "+  moment( result.fechaDespliegue ).format( 'DD/MM/YYYY' ));
+      });
   }
 }
