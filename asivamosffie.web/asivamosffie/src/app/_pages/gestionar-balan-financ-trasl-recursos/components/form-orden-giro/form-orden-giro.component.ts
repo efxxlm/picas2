95ef71b1fa191613c28f9810d662e7e767dc1779
Carlos Molero <cmorelo@ivolucion.com>
2022-06-23 16:13:18 -0500

    Fix contador traslados

--- asivamosffie.web/asivamosffie/src/app/_pages/gestionar-balan-financ-trasl-recursos/components/form-orden-giro/form-orden-giro.component.ts
+++ asivamosffie.web/asivamosffie/src/app/_pages/gestionar-balan-financ-trasl-recursos/components/form-orden-giro/form-orden-giro.component.ts
@@ -156,9 +156,10 @@ export class FormOrdenGiroComponent implements OnInit, OnChanges {
             )
         }
 
+        console.log( this.resultadoBusqueda.controls[ index ].get( 'check' ).value, this.ordenesGiro.controls, this.resultadoBusqueda )
         if ( this.resultadoBusqueda.controls[ index ].get( 'check' ).value === false ) {
             if ( this.ordenesGiro.length > 0 ) {
-                const ordenGiroIndex = this.ordenesGiro.controls.findIndex( control => control.get( 'contratacionProyectoId' ).value === this.resultadoBusqueda.controls[ index ].get( 'contratacionProyectoId' ).value );
+                const ordenGiroIndex = this.ordenesGiro.controls.findIndex( control => control.get( 'solicitudPagoId' ).value === this.resultadoBusqueda.controls[ index ].get( 'solicitudPagoId' ).value );
 
                 if ( ordenGiroIndex !== -1 ) {
                     this.ordenesGiro.removeAt( ordenGiroIndex );
